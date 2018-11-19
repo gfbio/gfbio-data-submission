5ce4f5abbaa918c7d77479d10ccd895536c72ec2
Marc Weber <maweber@mpi-bremen.de>
2018-11-19 13:06:02 +0100

    GFBUI-2165 clean submission_ui code from commented gdcj form stuff

--- gfbio_submissions/submission_ui/conf/settings.py
+++ gfbio_submissions/submission_ui/conf/settings.py
@@ -1,8 +1,1 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
-
-GCDJ_BASE_URL = getattr(
-    settings,
-    'GCDJ_BASE_URL',
-    'http://127.0.0.1:8000/{0}'
-)
--- gfbio_submissions/submission_ui/tests.py
+++ gfbio_submissions/submission_ui/tests.py
@@ -6,53 +6,6 @@ from django.conf import settings
 from django.test import TestCase
 
 
-# class CheckListValidationViewTest(TestCase):
-#
-#     def test_get(self):
-#         response = self.client.get('/validate_widget')
-#         self.assertEqual(302, response.status_code)
-#         self.assertEqual('widget', response['location'].split('/')[-1])
-#
-#     def test_post(self):
-#         data = {'gcdjson': '{"key": "value"}'}
-#         response = self.client.post('/validate_widget', data)
-#         self.assertEqual(200, response.status_code)
-#
-# class GcdjWidgetDeliverSchemaTest(TestCase):
-#
-#     def test_simple_get_schema(self):
-#         response = self.client.get('/widget_schemas/select')
-#         self.assertEqual(200, response.status_code)
-#
-#     def test_simple_get_options(self):
-#         response = self.client.get('/widget_options/select')
-#         self.assertEqual(200, response.status_code)
-#
-#     # TODO: move path to constant in dev_settings
-#     @mock.patch.object(settings, 'STATIC_ROOT', 'genomicsdataservices/gcdj_form/fixtures')
-#     def test_get_options_content(self):
-#         response = self.client.get('/widget_options/select')
-#         content = json.loads(response.content)
-#         self.assertIn('fields', content.keys())
-#
-#     def test_get_options_invalid_param(self):
-#         response = self.client.get('/widget_options/nope')
-#         content = json.loads(response.content)
-#         self.assertEqual({}, content)
-#
-#     # TODO: move path to constant in dev_settings
-#     @mock.patch.object(settings, 'STATIC_ROOT', 'genomicsdataservices/gcdj_form/fixtures')
-#     def test_get_schema_content(self):
-#         response = self.client.get('/widget_schemas/checklist')
-#         content = json.loads(response.content)
-#         self.assertIn('definitions', content.keys())
-#
-#     def test_get_schema_invalid_param(self):
-#         response = self.client.get('/widget_schemas/nope')
-#         content = json.loads(response.content)
-#         self.assertEqual({}, content)
-
-
 class EnaWidgetDelieverSchemasAndOptionsTest(TestCase):
 
     def test_simple_get_schema(self):
@@ -77,9 +30,8 @@ class EnaWidgetDelieverSchemasAndOptionsTest(TestCase):
     def test_get_options_content(self):
         response = self.client.get(
             '/brokerage/submissions/ena/form/options/sample')
-        print(response)
-        # content = json.loads(response.content)
-        # self.assertIn('fields', content.keys())
+        content = json.loads(response.content.decode('utf-8'))
+        self.assertIn('fields', content.keys())
 
     def test_get_options_invalid_param(self):
         response = self.client.get(
@@ -101,20 +53,3 @@ class EnaWidgetDelieverSchemasAndOptionsTest(TestCase):
             '/brokerage/submissions/ena/form/schema/nope')
         content = json.loads(response.content.decode('utf-8'))
         self.assertEqual({}, content)
-
-# class TestSomeNewStuff(TestCase):
-#     def test_app_config(self):
-#         from django.apps import apps, AppConfig
-#         app = Celery('genomicsdataservices')
-#         app.config_from_object('django.conf:settings')
-#         installed_apps = [app_config.name for app_config in
-#                           apps.get_app_configs()]
-#         self.assertTrue(True)
-
-# class EnaWidgetDeliverJavascriptViewTest(TestCase):
-#     @mock.patch.object(settings, 'STATIC_ROOT', 'genomicsdataservices/gcdj_form/fixtures')
-#     def test_get(self):
-#         response = self.client.get('/ena/form/js')
-#         print response.status_code
-#         print response.content
-#         # print response.content
