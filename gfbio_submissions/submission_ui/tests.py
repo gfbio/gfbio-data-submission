55529d610e743cde90c26c8591a25c599ef9c24a
Marc Weber <maweber@mpi-bremen.de>
2018-11-21 12:58:39 +0100

    GFBIO-2169 comment most code in submission_ui code, since it is obsolete but app needed

--- gfbio_submissions/submission_ui/tests.py
+++ gfbio_submissions/submission_ui/tests.py
@@ -6,50 +6,50 @@ from django.conf import settings
 from django.test import TestCase
 
 
-class EnaWidgetDelieverSchemasAndOptionsTest(TestCase):
-
-    def test_simple_get_schema(self):
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/schema/study')
-        self.assertEqual(200, response.status_code)
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/schema/non_sense')
-        self.assertEqual(200, response.status_code)
-
-    def test_simple_get_options(self):
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/options/sample')
-        self.assertEqual(200, response.status_code)
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/options/non_sense')
-        self.assertEqual(200, response.status_code)
-
-    # TODO: move path to constant in dev_settings
-    @mock.patch.object(settings, 'STATIC_ROOT',
-                       'gfbio_submissions/submission_ui/fixtures')
-    def test_get_options_content(self):
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/options/sample')
-        content = json.loads(response.content.decode('utf-8'))
-        self.assertIn('fields', content.keys())
-
-    def test_get_options_invalid_param(self):
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/options/nope')
-        content = json.loads(response.content.decode('utf-8'))
-        self.assertEqual({}, content)
-
-    # TODO: move path to constant in dev_settings
-    @mock.patch.object(settings, 'STATIC_ROOT',
-                       'gfbio_submissions/submission_ui/fixtures')
-    def test_get_schema_content(self):
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/schema/study')
-        content = json.loads(response.content.decode('utf-8'))
-        self.assertIn('properties', content.keys())
-
-    def test_get_schema_invalid_param(self):
-        response = self.client.get(
-            '/brokerage/submissions/ena/form/schema/nope')
-        content = json.loads(response.content.decode('utf-8'))
-        self.assertEqual({}, content)
+# class EnaWidgetDelieverSchemasAndOptionsTest(TestCase):
+#
+#     def test_simple_get_schema(self):
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/schema/study')
+#         self.assertEqual(200, response.status_code)
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/schema/non_sense')
+#         self.assertEqual(200, response.status_code)
+#
+#     def test_simple_get_options(self):
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/options/sample')
+#         self.assertEqual(200, response.status_code)
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/options/non_sense')
+#         self.assertEqual(200, response.status_code)
+#
+#     # TODO: move path to constant in dev_settings
+#     @mock.patch.object(settings, 'STATIC_ROOT',
+#                        'gfbio_submissions/submission_ui/fixtures')
+#     def test_get_options_content(self):
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/options/sample')
+#         content = json.loads(response.content.decode('utf-8'))
+#         self.assertIn('fields', content.keys())
+#
+#     def test_get_options_invalid_param(self):
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/options/nope')
+#         content = json.loads(response.content.decode('utf-8'))
+#         self.assertEqual({}, content)
+#
+#     # TODO: move path to constant in dev_settings
+#     @mock.patch.object(settings, 'STATIC_ROOT',
+#                        'gfbio_submissions/submission_ui/fixtures')
+#     def test_get_schema_content(self):
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/schema/study')
+#         content = json.loads(response.content.decode('utf-8'))
+#         self.assertIn('properties', content.keys())
+#
+#     def test_get_schema_invalid_param(self):
+#         response = self.client.get(
+#             '/brokerage/submissions/ena/form/schema/nope')
+#         content = json.loads(response.content.decode('utf-8'))
+#         self.assertEqual({}, content)
--- gfbio_submissions/submission_ui/urls.py
+++ gfbio_submissions/submission_ui/urls.py
@@ -7,14 +7,14 @@ from . import views
 
 urlpatterns = [
 
-    url(
-        regex=r'^brokerage/submissions/ena/form/schema/(?P<schema_name>[a-z\-_]+)$',
-        view=views.EnaWidgetDeliverSchemaView.as_view(),
-        name='ena_widget_schemas'
-    ),
-    url(
-        regex=r'^brokerage/submissions/ena/form/options/(?P<options_name>[a-z\-_]+)$',
-        view=views.EnaWidgetDeliverOptionsView.as_view(),
-        name='ena_widget_options'
-    ),
+    # url(
+    #     regex=r'^brokerage/submissions/ena/form/schema/(?P<schema_name>[a-z\-_]+)$',
+    #     view=views.EnaWidgetDeliverSchemaView.as_view(),
+    #     name='ena_widget_schemas'
+    # ),
+    # url(
+    #     regex=r'^brokerage/submissions/ena/form/options/(?P<options_name>[a-z\-_]+)$',
+    #     view=views.EnaWidgetDeliverOptionsView.as_view(),
+    #     name='ena_widget_options'
+    # ),
 ]
