cb95544882867f0fbad4ee3e2270a8a3a0edc556
Marc Weber <maweber@mpi-bremen.de>
2018-11-26 21:59:57 +0100

    develop de-comment content and serializer settings of celery

--- config/settings/base.py
+++ config/settings/base.py
@@ -285,8 +285,8 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 ########## CELERY
 INSTALLED_APPS += ['gfbio_submissions.taskapp.celery.CeleryConfig']
 CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='django://')
-# CELERY_TASK_SERIALIZER = 'json'
-# CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['application/json']
 
 # FROM gds common settings, why ? not in .env of gds
 # CELERY_RESULT_BACKEND = env('REDIS_URL')
