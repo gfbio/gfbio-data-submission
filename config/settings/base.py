ed90657d0fa935c3ec8220ac349914b094092fb4
Marc Weber <maweber@mpi-bremen.de>
2018-11-27 14:47:02 +0100

    release/1.60.6 bump version no.

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,7 +12,7 @@ from celery.schedules import crontab
 
 # VERSION NUMBER
 # ------------------------------------------------------------------------------#
-VERSION = '1.60.5'
+VERSION = '1.60.6'
 
 ROOT_DIR = environ.Path(
     __file__) - 3  # (gfbio_submissions/config/settings/base.py - 3 = gfbio_submissions/)
