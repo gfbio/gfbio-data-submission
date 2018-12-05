41f1dc7fbe2eb83b2a857ca6accbe723db427b77
Marc Weber <maweber@mpi-bremen.de>
2018-12-05 14:35:06 +0100

    GFBIO-2217 add test that uses a submission without submitting user

--- gfbio_submissions/brokerage/tests.py
+++ gfbio_submissions/brokerage/tests.py
@@ -5810,8 +5810,6 @@ class TestHelpDeskTicketMethods(TestCase):
         pd.comment = 'lorem ipsum'
         pd.save(attach=False)
 
-        primary_data_files = PrimaryDataFile.objects.all()
-
         request_logs = RequestLog.objects.all()
         self.assertEqual(0, len(request_logs))
         mock_requests.post.return_value.status_code = 200
@@ -5821,6 +5819,33 @@ class TestHelpDeskTicketMethods(TestCase):
         pd = sub.primarydatafile_set.first()
         response = gfbio_helpdesk_attach_file_to_ticket(sc, 'SAND-959',
                                                         pd.data_file, sub)
+        self.assertEqual(200, response.status_code)
+        request_logs = RequestLog.objects.all()
+        self.assertEqual(1, len(request_logs))
+
+    @patch('gfbio_submissions.brokerage.utils.gfbio.requests')
+    def test_attach_template_without_submitting_user(self, mock_requests):
+        sc = SiteConfiguration.objects.get(pk=1)
+        sub = Submission.objects.all().first()
+        pd = PrimaryDataFile()
+        pd.submission = sub
+        pd.site = sub.site
+        pd.comment = 'lorem ipsum'
+        pd.save(attach=False)
+        sub.submitting_user = None
+        sub.save()
+
+        print('sub.ssubmitting user ', sub.submitting_user)
+        request_logs = RequestLog.objects.all()
+        self.assertEqual(0, len(request_logs))
+
+        mock_requests.post.return_value.status_code = 200
+        mock_requests.post.return_value.ok = True
+        mock_requests.post.return_value.content = '[{"self": "https://helpdesk.gfbio.org/rest/api/2/attachment/10814", "id": "10814", "filename": "test_primary_data_file_TE4k513", "author": { "self": "https://helpdesk.gfbio.org/rest/api/2/user?username=brokeragent", "name": "brokeragent", "key": "brokeragent@gfbio.org", "emailAddress": "brokeragent@gfbio.org", "avatarUrls": { "48x48": "https://helpdesk.gfbio.org/secure/useravatar?avatarId=10349", "24x24": "https://helpdesk.gfbio.org/secure/useravatar?size=small&avatarId=10349", "16x16": "https://helpdesk.gfbio.org/secure/useravatar?size=xsmall&avatarId=10349", "32x32": "https://helpdesk.gfbio.org/secure/useravatar?size=medium&avatarId=10349"}, "displayName": "Broker Agent", "active": true, "timeZone": "Europe/Berlin"}, "created": "2017-06-19T09:23:43.000+0000", "size": 8, "content": "https://helpdesk.gfbio.org/secure/attachment/10814/test_primary_data_file_TE4k513"}]'
+        pd = sub.primarydatafile_set.first()
+        response = gfbio_helpdesk_attach_file_to_ticket(sc, 'SAND-959',
+                                                        pd.data_file, sub)
+
         self.assertEqual(200, response.status_code)
         request_logs = RequestLog.objects.all()
         self.assertEqual(1, len(request_logs))
