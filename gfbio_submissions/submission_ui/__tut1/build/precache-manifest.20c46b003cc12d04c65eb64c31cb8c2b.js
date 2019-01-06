a8e76dba6c24460f65efc41a283a3ff2163c87a1
Marc Weber <maweber@mpi-bremen.de>
2019-01-06 22:59:58 +0100

    Remove default example

---
+++ gfbio_submissions/submission_ui/__tut1/build/precache-manifest.20c46b003cc12d04c65eb64c31cb8c2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d83d21cfc22cbaf5e6d",
+    "url": "/static/js/main.4d83d21c.chunk.js"
+  },
+  {
+    "revision": "c64aed0e30b56d5e2e5d",
+    "url": "/static/js/1.c64aed0e.chunk.js"
+  },
+  {
+    "revision": "4d83d21cfc22cbaf5e6d",
+    "url": "/static/css/main.60c826c2.chunk.css"
+  },
+  {
+    "revision": "5e8f11a963d3a771789315b45fbaeafc",
+    "url": "/index.html"
+  }
+];
