ee931e3303b5fc8e077f5402c6957cfc65625edc
Marc Weber <maweber@mpi-bremen.de>
2019-01-27 20:50:30 +0100

    GFBIO-2292 add search template for comparision and testing

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    # TODO: just for comparision, perhaps search app is ported to submissions project
     url(r'^search/$', TemplateView.as_view(template_name='pages/search_results.html'), name='search'),
 
 
