ca8e0c2a01fa08f9450025527c7b77fc94947695
Marc Weber <maweber@mpi-bremen.de>
2019-02-06 11:57:55 +0100

    GFBIO-2295 add submission_mock.html template
    - add submission_ui/url.py entry

--- gfbio_submissions/submission_ui/urls.py
+++ gfbio_submissions/submission_ui/urls.py
@@ -4,6 +4,12 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(
+        r'submission/mock/$',
+        TemplateView.as_view(
+            template_name='submission_ui/submission_mock.html'),
+        name='submission_mock'
+    ),
+    url(
         # by omitting the '$' in the url,
         # the react app in this template will take care of every url below
         # more specific: the react-router checks for matches.
