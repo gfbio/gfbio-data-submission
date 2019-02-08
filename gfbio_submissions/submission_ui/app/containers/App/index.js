e83358cdd70f23cc0faec3c8b8fe1c6fa8b59f1e
Marc Weber <maweber@mpi-bremen.de>
2019-02-08 21:49:45 +0100

    GFBIO-2290 add redux-form reducer to application
    - add SubmissionForm route

--- gfbio_submissions/submission_ui/app/containers/App/index.js
+++ gfbio_submissions/submission_ui/app/containers/App/index.js
@@ -10,11 +10,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
-import Test_1 from 'containers/Test_1/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import SubmissionForm from 'containers/SubmissionForm/Loadable';
 
 // TODO: set according to root django url, full path from config/urls.py
-//  to APP/urls.py
+//  to APP/urls.py. local development works, but entering URL with prefix
+//  prevents error messages
 const urlPrefix = '/ui/submission';
 export default function App() {
   return (
@@ -22,7 +23,8 @@ export default function App() {
       <BrowserRouter basename={urlPrefix}>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/t1" component={Test_1} />
+          {/* TODO: maybe remove /form url and use / instead ? */}
+          <Route exact path="/form" component={SubmissionForm} />
           <Route component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
