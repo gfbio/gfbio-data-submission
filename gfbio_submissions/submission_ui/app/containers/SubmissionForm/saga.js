1d0fbb6b781c4892d738c3992a31d57e41f27237
Marc Weber <maweber@mpi-bremen.de>
2019-02-14 22:48:42 +0100

    GFBIO-2290 restructure saga export

--- gfbio_submissions/submission_ui/app/containers/SubmissionForm/saga.js
+++ gfbio_submissions/submission_ui/app/containers/SubmissionForm/saga.js
@@ -1,11 +1,31 @@
 import { take, call, put, select, takeLatest, all } from 'redux-saga/effects';
 import { SAVE_FORM, SUBMIT_FORM } from './constants';
-import { makeSelectFormWrapper, makeSelectReduxFormForm } from './selectors';
+import {
+  makeSelectFormWrapper,
+  makeSelectLicense,
+  makeSelectMetaDataSchema,
+  makeSelectReduxFormForm,
+} from './selectors';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export function* performSubmitFormSaga() {
   console.log('performSubmitFormSaga');
   const reduxFormForm = yield select(makeSelectReduxFormForm());
   console.log(reduxFormForm);
+  const license = yield select(makeSelectLicense());
+  const metaDataSchema = yield select(makeSelectMetaDataSchema());
+  console.log(`${license} ${metaDataSchema}`);
+  const data = Object.assign({ license, metaDataSchema }, reduxFormForm);
+  try {
+    // const response = yield call(sendMethod, param1, param2);
+    const response = yield call(sleep, 3000);
+    console.log('sent data');
+    console.log(data);
+  } catch (error) {
+    console.log('Error');
+    console.log(error);
+  }
   console.log('------ END performSubmitFormSaga -----');
 }
 
@@ -13,20 +33,28 @@ export function* performSaveFormSaga() {
   console.log('performSaveFormSaga');
   const reduxFormForm = yield select(makeSelectReduxFormForm());
   console.log(reduxFormForm);
-  const formWrapper = yield select(makeSelectFormWrapper());
-  console.log(formWrapper);
+  // const license = yield select(makeSelectLicense());
+  // const metaDataSchema = yield select(makeSelectMetaDataSchema());
+  // console.log(`${license} ${metaDataSchema}`);
+  // form state as it is, without submit needed
+  // const formWrapper = yield select(makeSelectFormWrapper());
+  // console.log(formWrapper);
   console.log('------ END performSaveFormSaga -----');
 }
 
 export function* processSubmitFormTypeSaga() {
   console.log('processSubmitFormType');
   const reduxFormForm = yield select(makeSelectReduxFormForm());
-  console.log(reduxFormForm);
+  // console.log(reduxFormForm);
   if (reduxFormForm.workflow === 'save') {
     console.log('DO SAVE');
     yield* performSaveFormSaga();
-  } else {
+  } else if (reduxFormForm.workflow === 'submit') {
     console.log('DO SUBMIT (else)');
+    // TODO: better fork saga ?
+    //  https://medium.freecodecamp.org/redux-saga-common-patterns-48437892e11c
+    //  https://decembersoft.com/posts/4-tips-for-managing-many-sagas-in-a-react-redux-saga-app/
+    //  https://mysticcoders.com/blog/simplifying-redux-saga-entry-file
     yield* performSubmitFormSaga();
   }
   console.log('------ END processSubmitFormType -----');
@@ -36,10 +64,10 @@ export function* submitFormSaga() {
   yield takeLatest(SUBMIT_FORM, processSubmitFormTypeSaga);
 }
 
-// export function* saveFormSaga() {
-//   yield takeLatest(SAVE_FORM, performSaveFormSaga);
-// }
+export function* saveFormSaga() {
+  yield takeLatest(SAVE_FORM, performSaveFormSaga);
+}
 
 export default function* rootSaga() {
-  yield all([submitFormSaga(), processSubmitFormTypeSaga()]);
+  yield all([submitFormSaga(), saveFormSaga()]);
 }
