5adfc5b47960d8746a65fc81721b57ee4ed86951
Marc Weber <maweber@mpi-bremen.de>
2019-03-01 15:20:58 +0100

    GFBIO-2338 add RelatedPublicationsForm component

---
+++ gfbio_submissions/submission_ui/app/components/RelatedPublicationsForm/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * RelatedPublicationsForm
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class RelatedPublicationsForm extends React.PureComponent {
+  render() {
+    return <div />;
+  }
+}
+
+RelatedPublicationsForm.propTypes = {};
+
+export default RelatedPublicationsForm;
