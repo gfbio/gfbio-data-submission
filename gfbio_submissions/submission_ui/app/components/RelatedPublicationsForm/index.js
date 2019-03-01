98a58d331cb9f07c12cf130d841d1c7cfe139c6f
Marc Weber <maweber@mpi-bremen.de>
2019-03-01 15:56:35 +0100

    GFBIO-2338 add textfield and button

--- gfbio_submissions/submission_ui/app/components/RelatedPublicationsForm/index.js
+++ gfbio_submissions/submission_ui/app/components/RelatedPublicationsForm/index.js
@@ -5,13 +5,61 @@
  */
 
 import React from 'react';
+import { Field } from 'redux-form/lib/immutable';
+import { required } from '../MinimalSubmissionForm/validation';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
 /* eslint-disable react/prefer-stateless-function */
 class RelatedPublicationsForm extends React.PureComponent {
+  // renderInputField = ({
+  //                       input,
+  //                       placeholder,
+  //                       type,
+  //                       meta: { touched, error, warning },
+  //                     }) => (
+  //   <div>
+  //     <div>
+  //       <input
+  //         {...input}
+  //         placeholder={placeholder}
+  //         type={type}
+  //         className="form-control"
+  //       />
+  //       {touched &&
+  //       ((error && <span className="input-error">{error}</span>) ||
+  //         (warning && <span className="input-warning">{warning}</span>))}
+  //     </div>
+  //   </div>
+  // );
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <header className="header header-left form-header-top">
+          <h2 className="section-title">Related Publications</h2>
+          <p className="section-subtitle">(optional)</p>
+        </header>
+        <div className="form-row">
+          <div className="form-group col-md-10">
+            <Field
+              name="relatedPublication"
+              className="form-control"
+              component="input"
+              type="text"
+              placeholder="Enter a publication or reference"
+            />
+          </div>
+          <div className="form-group col-md-2">
+            <button
+              className="btn btn-secondary btn-block
+              btn-light-blue-inverted">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
