e614cb34ebadaa3f12e7e209584e18150c37edb6
Marc Weber <maweber@mpi-bremen.de>
2019-03-20 21:54:47 +0100

    GFBIO-2331 de-comment add form fields

--- gfbio_submissions/submission_ui/app/components/ContributersForm/index.js
+++ gfbio_submissions/submission_ui/app/components/ContributersForm/index.js
@@ -186,101 +186,94 @@ class ContributersForm extends React.PureComponent {
             <div id="contributorFormWrapper" className="collapse"
                  aria-labelledby="headingOne" data-parent="#accordion">
 
-              <h3><i>tmp header form</i></h3>
-
               <div className="card card-body">
-                {/*<h5>Add Contributor</h5>*/}
-                {/*<div className="form-row">*/}
-                {/*<div className="form-group col-md-3">*/}
+                <h5>Add Contributor</h5>
+                <div className="form-row">
+                  <div className="form-group col-md-3">
 
-                {/*<label htmlFor="firstName">First Name</label>*/}
-                {/*<input type="text" className="form-control"*/}
-                {/*id="firstName" onChange={this.handleChange}*/}
-                {/*// required*/}
-                {/*/>*/}
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" className="form-control"
+                           id="firstName" onChange={this.handleChange}
+                      // required
+                    />
 
-                {/*</div>*/}
-                {/*<div className="form-group col-md-3">*/}
+                  </div>
+                  <div className="form-group col-md-3">
 
-                {/*<label htmlFor="lastName">Last Name</label>*/}
-                {/*<input type="text" className="form-control" id="lastName"*/}
-                {/*onChange={this.handleChange}*/}
-                {/*// required*/}
-                {/*/>*/}
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" className="form-control" id="lastName"
+                           onChange={this.handleChange}
+                      // required
+                    />
 
-                {/*</div>*/}
-                {/*<div className="form-group col-md-6">*/}
+                  </div>
+                  <div className="form-group col-md-6">
 
-                {/*<label htmlFor="emailAddress">Email Address</label>*/}
-                {/*<input*/}
-                {/*type="email"*/}
-                {/*className="form-control"*/}
-                {/*id="emailAddress"*/}
-                {/*placeholder="name@example.com"*/}
-                {/*onChange={this.handleChange}*/}
-                {/*// required*/}
-                {/*/>*/}
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="emailAddress"
+                      placeholder="name@example.com"
+                      onChange={this.handleChange}
+                      // required
+                    />
 
-                {/*</div>*/}
-                {/*</div>*/}
-                {/*<div className="form-row">*/}
-                {/*<div className="form-group col-md-6">*/}
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-md-6">
 
-                {/*<label htmlFor="institution">Institution (optional)</label>*/}
-                {/*<input*/}
-                {/*type="text"*/}
-                {/*className="form-control"*/}
-                {/*id="institution"*/}
-                {/*onChange={this.handleChange}*/}
-                {/*/>*/}
+                    <label htmlFor="institution">Institution (optional)</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="institution"
+                      onChange={this.handleChange}
+                    />
 
-                {/*</div>*/}
-                {/*<div className="form-group col-md-6">*/}
+                  </div>
+                  <div className="form-group col-md-6">
 
-                {/*<label htmlFor="contribution">Contribution*/}
-                {/*(optional)</label>*/}
-                {/*<input*/}
-                {/*type="text"*/}
-                {/*className="form-control"*/}
-                {/*id="contribution"*/}
-                {/*onChange={this.handleChange}*/}
-                {/*/>*/}
+                    <label htmlFor="contribution">Contribution
+                      (optional)</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="contribution"
+                      onChange={this.handleChange}
+                    />
 
-                {/*</div>*/}
-                {/*</div>*/}
-                {/*<div className="form-row">*/}
-                {/*<div className="form-group col-md-2">*/}
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-md-2">
 
-                <button
-                  className="btn btn-secondary btn-sm btn-block btn-light-blue-inverted"
-                  data-toggle="collapse"
-                  data-target="#contributorFormWrapper"
-                  aria-expanded="false"
-                  aria-controls="contributorFormWrapper"
-                >
-                  Cancel
-                </button>
+                    <button
+                      className="btn btn-secondary btn-sm btn-block btn-light-blue-inverted"
+                      data-toggle="collapse"
+                      data-target="#contributorFormWrapper"
+                      aria-expanded="false"
+                      aria-controls="contributorFormWrapper"
+                    >
+                      Cancel
+                    </button>
 
-                {/*</div>*/}
-                {/*<div className="form-group col-md-2">*/}
+                  </div>
+                  <div className="form-group col-md-2">
 
-                {/*/!*<button*!/*/}
-                {/*/!*className="btn btn-secondary btn-sm btn-block btn-light-blue-inverted">*!/*/}
-                {/*/!*Remove*!/*/}
-                {/*/!*</button>*!/*/}
+                  </div>
+                  <div className="form-group col-md-4" />
+                  <div className="form-group col-md-4">
 
-                {/*</div>*/}
-                {/*<div className="form-group col-md-4" />*/}
-                {/*<div className="form-group col-md-4">*/}
-
-                <button
-                  className="btn btn-secondary btn-sm btn-block btn-light-blue"
-                  // onClick={this.onSave}
-                >
-                  Save
-                </button>
-                {/*</div>*/}
-                {/*</div>*/}
+                    <button
+                      className="btn btn-secondary btn-sm btn-block btn-light-blue"
+                      onClick={this.onSave}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
 
