87fb53a9b15a60f3352cc60302f283e1fa22314a
Marc Weber <maweber@mpi-bremen.de>
2019-03-22 22:11:47 +0100

    GFBIO-2331 add ContributorInputSection component

---
+++ gfbio_submissions/submission_ui/app/components/ContributersForm/ContributorInputSection.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * ContributorInputSection
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class ContributorInputSection extends React.PureComponent {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+ContributorInputSection.propTypes = {};
+
+export default ContributorInputSection;
