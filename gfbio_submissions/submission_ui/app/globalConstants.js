2b072f973315491b798ffd2badbd0021758d93d9
Marc Weber <maweber@mpi-bremen.de>
2019-04-07 21:50:39 +0200

    develop set global API_ROOT to empty string for production system

--- gfbio_submissions/submission_ui/app/globalConstants.js
+++ gfbio_submissions/submission_ui/app/globalConstants.js
@@ -1,9 +1,9 @@
 // TODO: no api root needed if app is on same host with django
-// export const API_ROOT = '';
+export const API_ROOT = '';
 // TODO: elsewhere:
 // export const API_ROOT = 'https://submission.gfbio.org';
 // TODO: for rapid js standalone development, full servername is needed though.
-export const API_ROOT = 'http://0.0.0.0:8000';
+//export const API_ROOT = 'http://0.0.0.0:8000';
 
 // TODO: merge redundant code of api-access-code like in this file
 export const SUBMISSIONS = '/api/submissions/';
