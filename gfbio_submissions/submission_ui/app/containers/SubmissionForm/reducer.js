a5492c5999c6dc40635eee0e20c21623eb29bfb2
Marc Weber <maweber@mpi-bremen.de>
2019-05-06 14:52:09 +0200

    GFBIO-2462 add intial contributors as optional parameter to resetStateFormValues
    - call in reducer case

--- gfbio_submissions/submission_ui/app/containers/SubmissionForm/reducer.js
+++ gfbio_submissions/submission_ui/app/containers/SubmissionForm/reducer.js
@@ -269,7 +269,7 @@ function submissionFormReducer(state = initialState, action) {
       return state;
     case RESET_FORM:
       // console.log('RESET_FORM');
-      return resetStateFormValues(state);
+      return resetStateFormValues(state, getInitialContributors(backendParameters));
     case UPDATE_SUBMISSION:
       // console.log('UPDATE_SUBMISSION');
       // TODO: set prop to inidcate loading -> loading gif
