74d46a6808d46e9c43695c7b1cfc38a12fdad7a1
Marc Weber <maweber@mpi-bremen.de>
2019-05-27 14:07:26 +0200

    GFBIO-2437 style datepicker in modal

--- gfbio_submissions/submission_ui/app/components/EmbargoDatePicker/index.js
+++ gfbio_submissions/submission_ui/app/components/EmbargoDatePicker/index.js
@@ -24,6 +24,9 @@ import { compose } from 'redux';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import dateFormat from 'dateformat';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // import styled from 'styled-components';
 
 /* eslint-disable react/prefer-stateless-function */
@@ -54,26 +57,110 @@ class EmbargoDatePicker extends React.Component {
           backdrop={true}
           centered
         >
-          <Modal.Body>
-            <DatePicker
-              customInput={
-                <ButtonInput
-                  cssClassName="btn btn-secondary btn-block btn-light-blue"
-                  iconClassName="fa fa-calendar"
-                  text="Set Embargo"
-                />
-              }
-              inline
-              selected={this.props.embargoDate}
-              onChange={this.props.onChange}
-              dateFormat="MMMM d, yyyy"
-            />
-            <Button variant="secondary" className="btn-block green"
-                    onClick={this.props.closeEmbargoDialog}>
-              {/*<i className="icon ion-md-close" />*/}
-              CLOSE
-            </Button>
+
+
+          <Modal.Header className="text-center" closeButton>
+            <h4>Select Embargo Date</h4>
+          </Modal.Header>
+
+          <Modal.Body className="text-center">
+            <Container>
+              <Row className="show-grid">
+                <Col>
+                  <p>Current
+                    Embargo: <b>{dateFormat(this.props.embargoDate, 'dd mmmm yyyy')}</b>
+                  </p>
+                </Col>
+              </Row>
+
+              <Row className="show-grid mb-4">
+                <Col xs={12} md={4}>
+                  <Button variant="secondary"
+                          className="btn-sm btn-block btn-light-blue-inverted"
+                          onClick={this.props.closeEmbargoDialog}>
+                    {/*<i className="icon ion-md-close" />*/}
+                    6 months
+                  </Button>
+                </Col>
+                <Col xs={12} md={4}>
+                  <Button variant="secondary"
+                          className="btn-sm btn-block btn-light-blue-inverted"
+                          onClick={this.props.closeEmbargoDialog}>
+                    {/*<i className="icon ion-md-close" />*/}
+                    12 months
+                  </Button>
+                </Col>
+                <Col xs={12} md={4}>
+                  <Button variant="secondary"
+                          className="btn-sm btn-block btn-light-blue-inverted"
+                          onClick={this.props.closeEmbargoDialog}>
+                    {/*<i className="icon ion-md-close" />*/}
+                    18 months
+                  </Button>
+                </Col>
+              </Row>
+
+              <Row className="show-grid">
+                <Col xs={12} md={12}>
+                  <DatePicker
+                    customInput={
+                      <ButtonInput
+                        cssClassName="btn btn-secondary btn-block btn-light-blue"
+                        iconClassName="fa fa-calendar"
+                        text="Set Embargo"
+                      />
+                    }
+                    inline
+                    selected={this.props.embargoDate}
+                    onChange={this.props.onChange}
+                    dateFormat="MMMM d, yyyy"
+                  />
+                  {/*<Button variant="secondary" className="btn-block green"*/}
+                  {/*        onClick={this.props.closeEmbargoDialog}>*/}
+                  {/*  /!*<i className="icon ion-md-close" />*!/*/}
+                  {/*  CLOSE*/}
+                  {/*</Button>*/}
+                </Col>
+              </Row>
+            </Container>
           </Modal.Body>
+
+          <Modal.Footer>
+            <Container>
+              <Row className="show-grid">
+                <Col xs={12} md={6}>
+                  {/*<p>footer</p>*/}
+                  <Button variant="secondary"
+                          className="btn-sm btn-block btn-green-inverted"
+                          onClick={this.props.closeEmbargoDialog}>
+                    {/*<i className="icon ion-md-close" />*/}
+                    Cancel
+                  </Button>
+                </Col>
+                <Col xs={12} md={6}>
+                  <Button variant="secondary"
+                          className="btn-sm btn-block btn-green-inverted"
+                          onClick={this.props.closeEmbargoDialog}>
+                    {/*<i className="icon ion-md-calendar fa-lg align-top pr-2" />*/}
+                    Set Emargo
+                  </Button>
+                  {/*  <Button variant="secondary" className="btn-block green"*/}
+                  {/*          onClick={this.props.closeDeleteSubmissionDialog}>*/}
+                  {/*    <i className="icon ion-md-close" />*/}
+                  {/*    Cancel*/}
+                  {/*  </Button>*/}
+                  {/*</Col>*/}
+                  {/*<Col xs={12} md={6} className="text-right">*/}
+                  {/*  <Button variant="secondary" className="btn-block red"*/}
+                  {/*          onClick={this.props.deleteSubmission}>*/}
+                  {/*    <i className="icon ion-md-trash" />*/}
+                  {/*    Delete*/}
+                  {/*  </Button>*/}
+                </Col>
+              </Row>
+            </Container>
+          </Modal.Footer>
+
         </Modal>
       </div>
 
