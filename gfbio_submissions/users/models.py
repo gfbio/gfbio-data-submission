70110a6b004a51a727dc0e65ed5632fca441deb8
Marc Weber <maweber@mpi-bremen.de>
2019-08-02 14:47:39 +0200

    GFBIO-2448 migrate user model to include site and user fields
    - add migration file

--- gfbio_submissions/users/models.py
+++ gfbio_submissions/users/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    is_site = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=True)
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
