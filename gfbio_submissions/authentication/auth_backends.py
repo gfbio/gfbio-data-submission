c7b656a0ab3d4b3ce85f55f5aa729b13ae65bf98
Marc Weber <maweber@mpi-bremen.de>
2019-10-01 15:59:45 +0200

    GFBIO-2654 override get_username
    - save goesternid
    - update email in update_user method

--- gfbio_submissions/authentication/auth_backends.py
+++ gfbio_submissions/authentication/auth_backends.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 
+import unicodedata
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
-
-from gfbio_submissions.users.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -19,35 +18,20 @@ class GFBioAuthenticationBackend(OIDCAuthenticationBackend):
         logger.info('GFBioAuthenticationBackend | verify_claims | email={0}  | '
                     'verified={1}'.format(
             claims.get('email', 'NO_EMAIL_IN_CLAIM'), verified))
-        print('VERIFY_CLAIMS claims')
-        print(claims)
-        print()
+        # print('VERIFY_CLAIMS claims')
+        # print(claims)
+        # print()
         return verified
-
-    # VERIFY_CLAIMS claims
-    # django_1_b8ce6cc38c7a | {'sub': 'ivaylo.kostadinov@gwdg.de', 'email': 'ikostadi@gfbio.org', 'family_name': 'Kostadinov', 'given_name': 'Ivaylo', 'preferred_username': 'ivaylo.kostadinov', 'goe_id': '0404130'}
-    # django_1_b8ce6cc38c7a | INFO 2019-10-01 09:56:29,042 auth_backends 23 139730797817160 GFBioAuthenticationBackend | verify_claims | email=ikostadi@gfbio.org  | verified=True
-    # django_1_b8ce6cc38c7a | UDPATE_USER claims
-    # django_1_b8ce6cc38c7a | {'sub': 'ivaylo.kostadinov@gwdg.de', 'email': 'ikostadi@gfbio.org', 'family_name': 'Kostadinov', 'given_name': 'Ivaylo', 'preferred_username': 'ivaylo.kostadinov', 'goe_id': '0404130'}
-    # django_1_b8ce6cc38c7a | INFO 2019-10-01 09:56:29,052 auth_backends 23 139730797817160 GFBioAuthenticationBackend | update_user | email=ikostadi@gfbio.org  |
-    #
-    # django_1_b8ce6cc38c7a | INFO 2019-10-01 10:06:08,962 auth_backends 23 139730797817160 GFBioAuthenticationBackend | verify_claims | email=maweber@mpi-bremen.de  | verified=True
-    # django_1_b8ce6cc38c7a | VERIFY_CLAIMS claims
-    # django_1_b8ce6cc38c7a | {'sub': 'marc.weber01@gwdg.de', 'email': 'maweber@mpi-bremen.de', 'family_name': 'Weber', 'given_name': 'Marc', 'preferred_username': 'marc.weber01', 'goe_id': '0404134'}
-    # django_1_b8ce6cc38c7a |
-    # django_1_b8ce6cc38c7a | INFO 2019-10-01 10:06:08,974 auth_backends 23 139730797817160 GFBioAuthenticationBackend | update_user | email=maweber@mpi-bremen.de  |
-    # django_1_b8ce6cc38c7a | UDPATE_USER claims
-    # django_1_b8ce6cc38c7a | {'sub': 'marc.weber01@gwdg.de', 'email': 'maweber@mpi-bremen.de', 'family_name': 'Weber', 'given_name': 'Marc', 'preferred_username': 'marc.weber01', 'goe_id': '0404134'}
 
     # TODO:
     #   - check if gfbio sso, else default oidc for max. compatibility (curently only gfbio-sso via setings)
     #   - overide get_username ?
     #   - add additional generate_username algo for gfbio-sso, all others use generate_username with email
     # TODO:
-    #   - add geosternid att. to user (can be empty/Null)
+    #   - add geosternid att. to user (can be empty/Null) #
     #   - check here in create user, if existing map or error
     #   - now username is unique, email too ? email is mapped if existing ?
-    #   - if geostern id is used, than unique username should not be mandatory ?
+    #   - if geostern id is used, than unique username should not be mandatory ? #
     #   --> only one email for sso created users - replace existing one
     #   --> unique username for sso users too
 
@@ -60,13 +44,31 @@ class GFBioAuthenticationBackend(OIDCAuthenticationBackend):
     # signup -> "Dieser Benutzername ist bereits vergeben."
     #   1. nutzername ist unique, geht nicht anders
 
+    def get_username(self, claims):
+        print('MY GET_USERNAME ', claims)
+        # username_algo = self.get_settings('OIDC_USERNAME_ALGO', None)
+        #
+        # if username_algo:
+        #     if isinstance(username_algo, six.string_types):
+        #         username_algo = import_string(username_algo)
+        #     return username_algo(claims.get('email'))
+        # return default_username_algo(claims.get('email'))
+        username = claims.get(
+            'preferred_username',
+            claims.get('email')
+        )
+        return unicodedata.normalize('NFKC', username)[:150]
 
     # TODO: called on very first login via SSO
     def create_user(self, claims):
         user = super(GFBioAuthenticationBackend, self).create_user(claims)
+        user.goesternid = claims.get('goe_id', '')
         user.save()
         logger.info('GFBioAuthenticationBackend | create_user | email={0}  | '
-                    ''.format(claims.get('email', 'NO_EMAIL_IN_CLAIM')))
+                    'goesternid={1}'.format(
+            claims.get('email', 'NO_EMAIL_IN_CLAIM'),
+            claims.get('goesternid', 'NO_GOESTERNID_IN_CLAIM'))
+        )
         print('CREATE_USER claims')
         print(claims)
         print()
@@ -76,17 +78,11 @@ class GFBioAuthenticationBackend(OIDCAuthenticationBackend):
     def update_user(self, user, claims):
         user.first_name = claims.get('given_name', '')
         user.last_name = claims.get('family_name', '')
+        user.email = claims.get('email', '')
         user.save()
         logger.info('GFBioAuthenticationBackend | update_user | email={0}  | '
                     ''.format(claims.get('email', 'NO_EMAIL_IN_CLAIM')))
         print('UDPATE_USER claims')
         print(claims)
         print('user ', user, ' ', user.pk)
-        print('EMAILs')
-
-        # here only one per user I guess primary
-        emails = User.objects.filter(is_active=True).filter().values_list('email',
-                                                                 flat=True)
-        print(emails)
-        print()
         return user
