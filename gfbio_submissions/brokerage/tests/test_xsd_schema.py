a651b1ee736604b67d8c3394d2792f8a556740c2
Marc Weber <maweber@mpi-bremen.de>
2019-12-12 15:49:07 +0100

    GFBIO-2510 print assembled json to console

--- gfbio_submissions/brokerage/tests/test_xsd_schema.py
+++ gfbio_submissions/brokerage/tests/test_xsd_schema.py
@@ -1,5 +1,7 @@
+import json
 import os
 import xml.etree.ElementTree as ET
+from collections import OrderedDict
 
 from django.test import TestCase
 
@@ -9,6 +11,12 @@ from gfbio_submissions.brokerage.tests.utils import _get_test_data_dir_path
 class TestSubmissionAdmin(TestCase):
 
     def test_parse_xsd(self):
+
+        platform_definition_json = OrderedDict([
+            ('platform', OrderedDict([('oneOf', [])]))
+        ])
+        platform_instruments_json = OrderedDict()
+
         tree = ET.parse(
             os.path.join(
                 _get_test_data_dir_path(),
@@ -21,16 +29,30 @@ class TestSubmissionAdmin(TestCase):
             "*[@name='PlatformType']/{http://www.w3.org/2001/XMLSchema}choice")
 
         for r in platforms:
-            platform_name = r.attrib.get('name', '')
+            platform_name = r.attrib.get('name', '').lower()
+            platform_definition_json['platform']['oneOf'].append(
+                {
+                    '$ref': '#/{0}'.format(platform_name)
+                }
+            )
+
             instrument_model = r.find(
                 "{http://www.w3.org/2001/XMLSchema}complexType"
                 "/*/*[@name='INSTRUMENT_MODEL']").attrib.get('type', '').strip(
                 'com:')
 
-            print('\n------\nplatform: ', platform_name)
-            print('instrument: ', instrument_model)
+            platform_instruments_json[
+                '{0}'.format(platform_name)] = OrderedDict([
+                ('type', 'string'), ('enum', [])
+            ])
 
             instrument_variations = root.findall(
                 "*[@name='{0}']/*/*".format(instrument_model))
+
             for i in instrument_variations:
-                print(i.attrib.get('value', ''))
+                platform_instruments_json['{0}'.format(platform_name)][
+                    'enum'].append(i.attrib.get('value', ''))
+
+        print(json.dumps(platform_definition_json))
+        print('\n-----------------------------------\n')
+        print(json.dumps(platform_instruments_json))
