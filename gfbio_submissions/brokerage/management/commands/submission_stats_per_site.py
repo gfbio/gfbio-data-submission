ca0ab36cb1927c78659ffe3f3da89e2ab44e062b
Marc Weber <maweber@mpi-bremen.de>
2020-03-06 16:26:25 +0100

    develop add management command to show site related submission numbers

---
+++ gfbio_submissions/brokerage/management/commands/submission_stats_per_site.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from gfbio_submissions.brokerage.models import SiteConfiguration, Submission
+from gfbio_submissions.submission_ui.configuration.settings import HOSTING_SITE
+from gfbio_submissions.users.models import User
+
+
+class Command(BaseCommand):
+    help = 'shows total numbers of submissions vs. ' \
+           'submissions with sites gfbio-xxx & local'
+
+    def handle(self, *args, **kwargs):
+        all_submissions = len(Submission.objects.all())
+        gfbio_related_submissions = len(
+            Submission.objects.filter(site__username__contains='gfbio'))
+        local_site_submissions = len(
+            Submission.objects.filter(site__username=HOSTING_SITE))
+
+        print('\nsubmissions for sites')
+        print('\tall\tgfbio\tlocal-site')
+        print(
+            '\t{0}\t{1}\t{2}'.format(all_submissions, gfbio_related_submissions,
+                                     local_site_submissions))
+        print('------------------------------------------')
