863ec220350763a470271fe589c868bc7702a45b
Marc Weber <maweber@mpi-bremen.de>
2020-03-08 20:39:04 +0100

    develop add command to set submisson ownership (work in progress)

---
+++ gfbio_submissions/brokerage/management/commands/set_user_ownership.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from gfbio_submissions.brokerage.models import SiteConfiguration, Submission
+from gfbio_submissions.submission_ui.configuration.settings import HOSTING_SITE
+from gfbio_submissions.users.models import User
+
+
+class Command(BaseCommand):
+    help = 'shows total numbers of submissions vs. ' \
+           'submissions with sites gfbio-xxx & local'
+
+    def handle(self, *args, **kwargs):
+
+        gfbio_related_submissions = Submission.objects.filter(
+            site__username__contains='gfbio')
+        print('\n****************\tgfbio_related_submissions\t**************')
+        for submission in gfbio_related_submissions:
+            print('pk: ', submission.pk, ' | site: ', submission.site,
+                  ' | user: ', submission.user, ' | ',
+                  submission.submitting_user)
+
+        local_site_submissions = Submission.objects.filter(
+            site__username=HOSTING_SITE)
+        print('\n****************\tlocal_site_submissions\t**************')
+        for submission in local_site_submissions:
+            print('pk: ', submission.pk, ' | site: ', submission.site,
+                  ' | user: ', submission.user, ' | ',
+                  submission.submitting_user)
