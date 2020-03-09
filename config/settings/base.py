498fec9201f0bbbc04eae4097703a4971a34ddab
Marc Weber <maweber@mpi-bremen.de>
2020-03-09 15:09:59 +0100

    release/1.92.0 bump version

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,7 @@ import environ
 
 # VERSION NUMBER
 # ------------------------------------------------------------------------------#
-VERSION = '1.91.0'
+VERSION = '1.92.0'
 
 ROOT_DIR = (
         environ.Path(__file__) - 3
