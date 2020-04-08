6c99c079e7baa24971990c73e3fc87f1587030bf
Deniss Marinuks <d.marinuks@jacobs-university.de>
2020-04-08 10:27:43 +0200

    317-gitignore-and-ci-update
    - added gulp dependency
    - updated gulpfile.js
    - updated gitignore

--- userinterface/copy_layout_to_static.sh
+++ userinterface/copy_layout_to_static.sh
@@ -4,6 +4,5 @@ cd ./layout_playground/ &&
 gulp && cp -r vendor/themify-icons/ ../../gfbio_submissions/static/vendor/ &&
 # cp -r images ../../gfbio_submissions/static/ &&
 cp css/project.min.css ../../gfbio_submissions/static/css/project.css  &&
-cp js/project.min.js ../../gfbio_submissions/static/js/project.js &&
-cd ../../ && echo yes | docker-compose -f local.yml run --rm django python manage.py collectstatic
+cp js/project.min.js ../../gfbio_submissions/static/js/project.js
 
