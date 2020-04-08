f504969d81b9bb85590ed9c0b3c93205fff5da7d
Deniss Marinuks <d.marinuks@jacobs-university.de>
2020-04-08 11:59:28 +0200

    - keeping css and js empty folder - web-test

--- userinterface/copy_layout_to_static.sh
+++ userinterface/copy_layout_to_static.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ./layout_playground/ &&
+gulp &&
 # currently only themify icons need to be stored locally
-gulp && cp -r vendor/themify-icons/ ../../gfbio_submissions/static/vendor/ &&
+cp -r vendor/themify-icons/ ../../gfbio_submissions/static/vendor/ &&
 # cp -r images ../../gfbio_submissions/static/ &&
 cp css/project.min.css ../../gfbio_submissions/static/css/project.css  &&
 cp js/project.min.js ../../gfbio_submissions/static/js/project.js
