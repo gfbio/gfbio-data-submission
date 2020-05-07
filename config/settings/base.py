315d8b853f2375d44b21b984515a0a905b031a24
marc.weber01 <maweber@mpi-bremen.de>
2020-05-07 15:58:46 +0200

    master bump version no

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,7 @@ import environ
 
 # VERSION NUMBER
 # ------------------------------------------------------------------------------#
-VERSION = '1.98.0'
+VERSION = '1.99.0'
 
 ROOT_DIR = (
         environ.Path(__file__) - 3
