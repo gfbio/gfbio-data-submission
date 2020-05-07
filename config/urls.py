53431acc5eda130861edf12620641e5a99e8c0d3
Deniss Marinuks <d.marinuks@jacobs-university.de>
2020-05-07 12:31:22 +0200

    342-logo-link-and-redirect
    - added link to GFBio logo on home page
    - auto redirect if user is logged in
    - web-test

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,10 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
+from gfbio_submissions.brokerage.views import HomeView
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", HomeView.as_view(), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
