765703905a3e050f4c9f2239bd348ebf47171f3d
marc.weber01 <maweber@mpi-bremen.de>
2020-05-14 11:26:52 +0200

    352 remove url listfilter for requestlogs
    - add some comments

--- gfbio_submissions/brokerage/api/issue_update_views.py
+++ gfbio_submissions/brokerage/api/issue_update_views.py
@@ -8,6 +8,27 @@ from gfbio_submissions.generic.models import RequestLog
 from gfbio_submissions.generic.serializers import JiraHookRequestSerializer
 
 
+# "webhookEvent": "jira:issue_updated",
+# "issue_event_type_name": "issue_updated",
+
+# TODO: present when logged in ? test when logged in as brokeragent
+# tODO: release to develop. maybe issues whith host permissions
+# "user": {
+#     "self": "https://helpdesk.gfbio.org/rest/api/2/user?username=maweber%40mpi-bremen.de",
+#     "name": "maweber@mpi-bremen.de",
+#     "key": "maweber@mpi-bremen.de",
+#     "emailAddress": "maweber@mpi-bremen.de",
+#     "avatarUrls": {
+#         "48x48": "https://helpdesk.gfbio.org/secure/useravatar?avatarId=10335",
+#         "24x24": "https://helpdesk.gfbio.org/secure/useravatar?size=small&avatarId=10335",
+#         "16x16": "https://helpdesk.gfbio.org/secure/useravatar?size=xsmall&avatarId=10335",
+#         "32x32": "https://helpdesk.gfbio.org/secure/useravatar?size=medium&avatarId=10335"
+#     },
+#     "displayName": "Marc Weber",
+#     "active": true,
+#     "timeZone": "Europe/Berlin"
+# },
+
 class JiraIssueUpdateView(mixins.CreateModelMixin, generics.GenericAPIView):
     permission_classes = (permissions.APIAllowedHosts,)
     serializer_class = JiraHookRequestSerializer
