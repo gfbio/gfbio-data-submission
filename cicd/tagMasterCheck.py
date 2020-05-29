5ce73f775fd4a69b8069089af4b23ef8c45e22be
Deniss Marinuks <d.marinuks@jacobs-university.de>
2020-05-29 21:37:34 +0200

    update live server if tag found in master branch

--- cicd/tagMasterCheck.py
+++ cicd/tagMasterCheck.py
@@ -4,16 +4,15 @@ import os
 import sys
 import subprocess
 
-# get commit SHA
+# get first 8 chars of commit SHA
 COMMIT_SHA = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')[:8]
 
 # get commit reference
 headers = {'PRIVATE-TOKEN': os.environ['PRIVATE_TOKEN'] }
 r = requests.get("https://gitlab.gwdg.de/api/v4/projects/{}/repository/commits/{}/refs".format(os.environ['CI_PROJECT_ID'], COMMIT_SHA), headers=headers)
 
-isMaster = False
-branchCount = 0
 data = r.json()
+isMaster = False
 allBranches = 'Tag found in: '
 
 if 'message' in data:
@@ -22,12 +21,11 @@ if 'message' in data:
 
 for obj in data:
   if obj['type'] == 'branch':
-      branchCount += 1
       allBranches += '{} '.format(obj['name'])
       if obj['name'] == 'master':
           isMaster = True
 
-if isMaster and branchCount == 1:
+if isMaster:
     print(1)
 else:
-    print(allBranches)
+    print('Tag not found in master, found in: {}'.format(allBranches))
