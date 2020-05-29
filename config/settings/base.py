0b814e8a728d69880d4cce49af160202815de332
marc.weber01 <maweber@mpi-bremen.de>
2020-05-29 11:08:34 +0200

    master bump version to 1.102.0

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,7 +8,7 @@ import environ
 
 # VERSION NUMBER
 # ------------------------------------------------------------------------------#
-VERSION = '1.101.7'
+VERSION = '1.102.0'
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # gfbio_submissions/
