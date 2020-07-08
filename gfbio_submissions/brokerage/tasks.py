8df0c0073a18108fdbe8eca5818c2ec0d2ffeb57
marc.weber01 <maweber@mpi-bremen.de>
2020-07-08 16:21:20 +0200

    452 add check_for_user_without_site_configuration_task
    - add logging
    - extend testdata
    - add test for task
    - add subject template constant

--- gfbio_submissions/brokerage/tasks.py
+++ gfbio_submissions/brokerage/tasks.py
@@ -20,7 +20,8 @@ from .configuration.settings import ENA, ENA_PANGAEA, PANGAEA_ISSUE_VIEW_URL, \
     JIRA_FALLBACK_EMAIL, APPROVAL_EMAIL_SUBJECT_TEMPLATE, \
     APPROVAL_EMAIL_MESSAGE_TEMPLATE, JIRA_ACCESSION_COMMENT_TEMPLATE, \
     NO_HELPDESK_ISSUE_EMAIL_SUBJECT_TEMPLATE, \
-    NO_HELPDESK_ISSUEE_EMAIL_MESSAGE_TEMPLATE
+    NO_HELPDESK_ISSUEE_EMAIL_MESSAGE_TEMPLATE, \
+    NO_SITE_CONFIG_EMAIL_SUBJECT_TEMPLATE
 from .configuration.settings import SUBMISSION_MAX_RETRIES, \
     SUBMISSION_RETRY_DELAY
 from .exceptions import TransferServerError, TransferClientError
@@ -1489,6 +1490,9 @@ def check_for_submissions_without_helpdesk_issue_task(self):
     TaskProgressReport.objects.create_initial_report(
         submission=None,
         task=self)
+    logger.info(
+        msg='tasks.py | check_for_submissions_without_helpdesk_issue_task |'
+            ' start search')
     submissions_without_issue = Submission.objects.get_submissions_without_primary_helpdesk_issue()
     for sub in submissions_without_issue:
         logger.info(
@@ -1502,4 +1506,39 @@ def check_for_submissions_without_helpdesk_issue_task(self):
                 sub.broker_submission_id, sub.user.username
             )
         )
+    return True
+
+
+@celery.task(
+    base=SubmissionTask,
+    bind=True,
+    name='tasks.check_for_user_without_site_configuration_task',
+)
+def check_for_user_without_site_configuration_task(self):
+    TaskProgressReport.objects.create_initial_report(
+        submission=None,
+        task=self)
+    logger.info(
+        msg='tasks.py | check_for_user_without_site_configuration_task | start search')
+    users_without_config = User.objects.filter(is_user=True,
+                                               site_configuration=None)
+    site_config = SiteConfiguration.objects.get_hosting_site_configuration()
+    mail_content = 'Users without site_configuration found:'
+    for u in users_without_config:
+        logger.info(
+            msg='tasks.py | check_for_user_without_site_configuration_task | '
+                'found user {0} without site_configuration | '
+                'assign site_configuration'
+                ' {1}'.format(u.username, site_config.title))
+        u.site_configuration = site_config
+        u.save()
+        mail_content += '\nusername: {0}\temail: {1}\tpk: {2}'.format(
+            u.username, u.email, u.pk)
+    mail_content += '\nSite_configuration {0} was assigned automatically'.format(
+        site_config.title)
+    if len(users_without_config):
+        mail_admins(
+            subject=NO_SITE_CONFIG_EMAIL_SUBJECT_TEMPLATE.format(
+                len(users_without_config)),
+            message=mail_content)
     return True
