504f22fab97fda80ccc1d8412bc43f2c9de49e8e
marc.weber01 <maweber@mpi-bremen.de>
2020-09-03 10:40:39 +0200

    271 wip

--- gfbio_submissions/generic/forms.py
+++ gfbio_submissions/generic/forms.py
@@ -10,4 +10,11 @@ class ValidationSchemaSelectionForm(forms.Form):
             ('common_requirements_definitions.json',
              'common_requirements_definitions'),
             ('ena_study_definitions.json', 'ena_study_definitions'),
+
+            ('ena_sample_definitions.json', 'ena_sample_definitions'),
+            ('ena_experiment_definitions.json', 'ena_experiment_definitions'),
+            ('ena_run_definitions.json', 'ena_run_definitions'),
+            ('technical_content_definitions.json',
+             'technical_content_definitions'),
+
         ])
