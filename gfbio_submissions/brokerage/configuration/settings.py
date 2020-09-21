c33e91e3ef58b50c78bccf6290c357edd8229467
marc.weber01 <maweber@mpi-bremen.de>
2020-09-21 10:34:48 +0200

    553 set default for DEFAULT_ENA_CENTER_NAME to empty string

--- gfbio_submissions/brokerage/configuration/settings.py
+++ gfbio_submissions/brokerage/configuration/settings.py
@@ -115,7 +115,7 @@ DEFAULT_ENA_BROKER_NAME = getattr(
 DEFAULT_ENA_CENTER_NAME = getattr(
     settings,
     'DEFAULT_ENA_CENTER_NAME',
-    '_NO_CENTER_NAME_SET_'
+    ''
 )
 
 ENA_PANGAEA = getattr(
