da81daec1c2cd0b23227662c308fa1fc2714d478
marc.weber01 <maweber@mpi-bremen.de>
2020-09-21 11:46:16 +0200

    569 overwrite AccountAdapter save_user to add site_configuration

--- gfbio_submissions/users/adapters.py
+++ gfbio_submissions/users/adapters.py
@@ -5,10 +5,19 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
 from django.http import HttpRequest
 
+from gfbio_submissions.generic.models import SiteConfiguration
+
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
+    def save_user(self, request, user, form, commit=False):
+        user = super(AccountAdapter, self).save_user(
+            request, user, form, commit=False)
+        user.site_configuration = SiteConfiguration.objects.get_hosting_site_configuration()
+        user.save()
+        return user
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
