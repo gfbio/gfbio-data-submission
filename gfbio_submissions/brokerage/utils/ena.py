96a647e3e5c70e03c5643650f59f714f3b256adc
marc.weber01 <maweber@mpi-bremen.de>
2020-12-07 20:57:40 +0100

    718 adapt admin mail text for failing tasks

--- gfbio_submissions/brokerage/utils/ena.py
+++ gfbio_submissions/brokerage/utils/ena.py
@@ -961,30 +961,3 @@ def execute_update_accession_objects_chain(name_on_error=''):
      | update_resolver_accessions_task.s().set(countdown=SUBMISSION_DELAY) \
      | update_persistent_identifier_report_status_task.s().set(
                 countdown=SUBMISSION_DELAY))()
-
-    # FIXME: Prototype
-    # TODO: exceptions, logging, protocoll for curator
-    # TODO: move to task/celeryworkers
-    # def cli_call():
-    #     print('cli_call')
-    #     res = subprocess.run(['ls', '-l'], capture_output=True, check=True)
-    #     print('\n', res)
-    #
-    #     try:
-    #         res = subprocess.run(['java', '--version'], capture_output=True,
-    #                              check=True)
-    #         print('\n', res)
-    #     except subprocess.CalledProcessError as e:
-    #         print('error ', e)
-    #     except FileNotFoundError as e:
-    #         print('fnferror ', e)
-    #     try:
-    #         res = subprocess.run(
-    #             ['java', '-jar', 'ena_webin_cli/webin-cli-3.0.0.jar'],
-    #             capture_output=True,
-    #             check=False)
-    #         print('\n', res)
-    #     except subprocess.CalledProcessError as e:
-    #         print('error ', e)
-    #     except FileNotFoundError as e:
-    #         print('fnferror ', e)
