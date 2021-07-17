13af263c08ff7bff79527ed8bb1c8eb20113550f
Deniss Marinuks <dmarinuks@gmail.com>
2021-07-17 12:29:30 +0200

    907-post-welcome-msg
    - updated stack removal script

--- cicd/stopDockerStack.py
+++ cicd/stopDockerStack.py
@@ -10,7 +10,11 @@ if len(branch) == 0:
     print("COMMIT_MSG: ", commit_msg)
     sys.exit("branch name not found")
 
-bashCmd = "docker stack rm {}".format(branch[0])
+issue_id = re.findall("([0-9]+)-.+", branch[0])
+if len(issue_id) == 0:
+    sys.exit("issueID name not found")
+
+bashCmd = "docker stack rm {}".format(issue_id[0])
 process = subprocess.Popen(bashCmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = process.communicate()
 print("RESULT: ", stdout)
