1d66a9639f655cc618f1842fd6eeb81712ca4b4d
barbara.wagner <bwagner@gfbio.org>
2022-05-06 11:41:29 +0200

    912 reporter changed message and celery task created

--- gfbio_submissions/brokerage/tasks.py
+++ gfbio_submissions/brokerage/tasks.py
@@ -2355,6 +2355,62 @@ def notify_user_embargo_changed_task(self, prev=None, submission_id=None):
 @celery.task(
     base=SubmissionTask,
     bind=True,
+    name='tasks.notify_user_reporter_changed_task',
+    autoretry_for=(TransferServerError,
+                   TransferClientError
+                   ),
+    retry_kwargs={'max_retries': SUBMISSION_MAX_RETRIES},
+    retry_backoff=SUBMISSION_RETRY_DELAY,
+    retry_jitter=True
+)
+def notify_user_reporter_changed_task(self, prev=None, submission_id=None):
+    logger.info(
+        'tasks.py | notify_user_reporter_changed_task | submission_id={0}'.format(
+            submission_id))
+
+    submission, site_config = get_submission_and_site_configuration(
+        submission_id=submission_id,
+        task=self,
+        include_closed=True
+    )
+    if submission == TaskProgressReport.CANCELLED:
+        return TaskProgressReport.CANCELLED
+
+    if site_config and site_config.helpdesk_server:
+        reference = submission.get_primary_helpdesk_reference()
+        if reference:
+            comment = get_jira_comment_template(
+                template_name="NOTIFY_REPORTER_CHANGED",
+                task_name="notify_user_reporter_changed_task")
+            if not comment:
+                return TaskProgressReport.CANCELLED
+
+            comment = jira_comment_replace(
+                comment=comment,
+                username=submission.user)
+
+            jira_client = JiraClient(resource=site_config.helpdesk_server)
+            jira_client.add_comment(
+                key_or_issue=reference.reference_key,
+                text=comment,
+                is_internal=False
+            )
+            return jira_error_auto_retry(jira_client=jira_client, task=self,
+                                         broker_submission_id=submission.broker_submission_id)
+    else:
+        logger.info(
+            'tasks.py | notify_user_embargo_changed_task | no site_config for helpdesk_serever')
+
+    return {
+        'status': 'error',
+        'submission': '{}'.format(submission.broker_submission_id),
+        'msg': 'missing site_config or jira ticket'
+    }
+
+
+@celery.task(
+    base=SubmissionTask,
+    bind=True,
     name='tasks.jira_cancel_issue_task',
     autoretry_for=(TransferServerError,
                    TransferClientError
