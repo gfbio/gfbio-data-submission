352599e87cefbe27dac672b35aed97fa524c4d83
barbara.wagner <bwagner@gfbio.org>
2022-12-15 15:10:48 +0100

    'test_validate_atax_json' reset to specimen, no files

--- gfbio_submissions/brokerage/tests/test_utils/test_csv_parsing.py
+++ gfbio_submissions/brokerage/tests/test_utils/test_csv_parsing.py
@@ -1237,6 +1237,7 @@ class TestCSVParsing(TestCase):
     #     #
     #     # TODO: defaults to ; ok ! split to delim and do list comparision. done ...
 
+
     def test_validate_atax_json(self):
         import json
         data = {
@@ -1256,13 +1257,6 @@ class TestCSVParsing(TestCase):
                         'Basis of record': 'Preserved Specimen',
                         'Scientific name': 'Platypelis laetus'
                     },
-                ],
-                'atax_files': [{
-                        'Specimen identifier': 'ZSM 5652/2012',
-                        'IPR': 'Andolalao Rakotoarison',
-                        'File type': 'Image',
-                        'File name': 'Holotype_FGZC3761.jpg'
-                    }
                 ]
             }
         }
@@ -1270,93 +1264,12 @@ class TestCSVParsing(TestCase):
         jsonStr = json.dumps(data)
 
         # Checking type of object returned by json.dumps
-        t1 = type(jsonStr)
+        #t1 = type(jsonStr)
 
         my_obj = json.loads(jsonStr)
 
         # Checking type of object returned by json.loads
-        t2 = type(my_obj)
-
-        valid, errors = validate_data_full(my_obj, ATAX, None)
-
-        self.assertTrue(valid)
-
-
-    def test_validate_atax_files_json(self):
-        import json
-        data = {
-            'requirements': {
-                'atax_specimens': [{
-                    'Specimen identifier': 'ZSM 5652/2012',
-                    'Basis of record': 'Preserved Specimen',
-                    'Scientific name': 'Platypelis laetus'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5651/2012',
-                        'Basis of record': 'Preserved Specimen',
-                        'Scientific name': 'Platypelis laetus'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5653/2012',
-                        'Basis of record': 'Preserved Specimen',
-                        'Scientific name': 'Platypelis laetus'
-                    },
-                ],
-                'atax_files': [{
-                    'Specimen identifier': 'ZSM 5652/2012',
-                    'IPR': 'Andolalao Rakotoarison',
-                    'File type': 'Image',
-                    'File name': 'Holotype_FGZC3761.jpg'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5652/2012',
-                        'IPR': 'Frank Glaw',
-                        'File type': 'Image',
-                        'File name': '_MAD2789.tif'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5651/2012',
-                        'IPR': 'Andolalao Rakotoarison',
-                        'File type': 'Image',
-                        'File name': 'FGZC 3588.jpg'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5651/2012',
-                        'IPR': 'Andolalao Rakotoarison',
-                        'File type': 'Image',
-                        'File name': 'FGZC 3588.jpg_ventral'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5653/2012',
-                        'IPR': 'Andolalao Rakotoarison',
-                        'File type': 'Image',
-                        'File name': 'FGZC 3762.jpg'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5653/2012',
-                        'IPR': 'Andolalao Rakotoarison',
-                        'File type': 'Image',
-                        'File name': 'FGZC 3762_ventral.jpg'
-                    },
-                    {
-                        'Specimen identifier': 'ZSM 5651/2012',
-                        'IPR': 'Miguel Vences',
-                        'File type': 'Image',
-                        'File name': 'Platypelis_Sorata_plates_01July2019..jpg'
-                    }
-                ],
-            }
-        }
-
-        jsonStr = json.dumps(data)
-
-        # Checking type of object returned by json.dumps
-        t1 = type(jsonStr)
-
-        my_obj = json.loads(jsonStr)
-
-        # Checking type of object returned by json.loads
-        t2 = type(my_obj)
+        #t2 = type(my_obj)
 
         valid, errors = validate_data_full(my_obj, ATAX, None)
 
