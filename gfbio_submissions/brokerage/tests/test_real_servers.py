e968fcee70df390706e7fe066e6568625ae16d58
Marc Weber <mweber@gfbio.org>
2023-09-07 13:49:31 +0200

    DASS-1118 remove readonly constraint from jiramessage admin
    - put obsolete template settings in comments

--- gfbio_submissions/brokerage/tests/test_real_servers.py
+++ gfbio_submissions/brokerage/tests/test_real_servers.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import pprint
 import time
+from pprint import pp, pprint
 from unittest import skip
 
 import requests
@@ -97,3 +99,81 @@ class TestSubmissionServers(TestCase):
             headers=headers
         )
         content = json.loads(response.content.decode('utf-8'))
+
+
+class TestSubmissionServersForAtaxWorkflow(TestCase):
+
+    @skip('test against submissions.gfbio.dev')
+    def test_post_with_atax_target(self):
+        # TOKEN for user marc6
+        headers = {
+            'Authorization': 'Token x',
+            'Content-Type': 'application/json'
+        }
+
+        # --------------------------------------------------------------------------------
+
+        title = 'Test from local unit-test-environment {0}'.format(
+            time.strftime('%X %x %Z'))
+        data = {
+            'target': 'ATAX', 'release': False,
+            'data': {
+                'requirements': {
+                    'title': title,
+                    'description': 'A Description',
+                }
+            }
+        }
+
+        response = requests.post(
+            url='https://submissions.gfbio.dev/api/submissions/',
+            data=json.dumps(data),
+            headers=headers
+        )
+
+        content = json.loads(response.content.decode('utf-8'))
+        print(response.status_code)
+        pprint(content)
+
+        # --------------------------------------------------------------------------------
+
+        # broker_submission_id = content['broker_submission_id']
+        #
+        # files = {
+        #     'file': open(
+        #         os.path.join(
+        #             _get_test_data_dir_path(),
+        #             'csv_files/molecular_metadata.csv'),
+        #         'rb'
+        #     )
+        # }
+        # url = 'https://c103-171.cloud.gwdg.de/api/submissions/' \
+        #       '{0}/upload/'.format(broker_submission_id)
+        # response = requests.post(
+        #     url=url,
+        #     files=files,
+        #     data={'meta_data': True},
+        #     headers={
+        #         'Authorization': 'Token a---',
+        #     }
+        # )
+        # content = json.loads(response.content.decode('utf-8'))
+        # data = {
+        #     'target': 'GENERIC', 'release': True,
+        #     'submitting_user': '3',  # marc
+        #     'data': {
+        #         'requirements': {
+        #             'title': title,
+        #             'description': 'A Description',
+        #             'data_center': 'ENA – European Nucleotide Archive',
+        #         }
+        #     }
+        #
+        # }
+        # response = requests.put(
+        #     url='https://c103-171.cloud.gwdg.de/api/submissions/{0}/'.format(
+        #         broker_submission_id),
+        #     data=json.dumps(data),
+        #     headers=headers
+        # )
+        # content = json.loads(response.content.decode('utf-8'))
