c8ee23889722e44253e8539d964d95893724f1b7
Marc Weber <mweber@gfbio.org>
2023-09-19 20:18:58 +0200

    master put duplicate task in comments

--- gfbio_submissions/brokerage/tasks.py
+++ gfbio_submissions/brokerage/tasks.py
@@ -2906,35 +2906,35 @@ def atax_submission_combine_xmls_to_one_structure_task(self, previous_task_resul
 
         return True
 
-
-@app.task(
-    base=SubmissionTask,
-    bind=True,
-    name='tasks.prepare_ena_submission_data_task',
-)
-def prepare_ena_submission_data_task(self, prev_task_result=None,
-                                     submission_id=None):
-    submission, site_configuration = get_submission_and_site_configuration(
-        submission_id=submission_id,
-        task=self,
-        include_closed=True
-    )
-    if submission == TaskProgressReport.CANCELLED:
-        return TaskProgressReport.CANCELLED
-
-    if len(submission.brokerobject_set.all()) > 0:
-        with transaction.atomic():
-            submission.auditabletextdata_set.all().delete()
-        ena_submission_data = prepare_ena_data(submission=submission)
-        store_ena_data_as_auditable_text_data(submission=submission,
-                                              data=ena_submission_data)
-        # TODO: this will become obsolete once, data is taken from AuditableTextData ....
-        return ena_submission_data
-    else:
-        logger.info(
-            msg='prepare_ena_submission_data_task. no brokerobjects. '
-                'return={0} '
-                'submission_id={1}'.format(TaskProgressReport.CANCELLED,
-                                           submission_id)
-        )
-        return TaskProgressReport.CANCELLED
+# FIXME: WTF, why is there a duplicate of a complete task ?
+# @app.task(
+#     base=SubmissionTask,
+#     bind=True,
+#     name='tasks.prepare_ena_submission_data_task',
+# )
+# def prepare_ena_submission_data_task(self, prev_task_result=None,
+#                                      submission_id=None):
+#     submission, site_configuration = get_submission_and_site_configuration(
+#         submission_id=submission_id,
+#         task=self,
+#         include_closed=True
+#     )
+#     if submission == TaskProgressReport.CANCELLED:
+#         return TaskProgressReport.CANCELLED
+#
+#     if len(submission.brokerobject_set.all()) > 0:
+#         with transaction.atomic():
+#             submission.auditabletextdata_set.all().delete()
+#         ena_submission_data = prepare_ena_data(submission=submission)
+#         store_ena_data_as_auditable_text_data(submission=submission,
+#                                               data=ena_submission_data)
+#         # TODO: this will become obsolete once, data is taken from AuditableTextData ....
+#         return ena_submission_data
+#     else:
+#         logger.info(
+#             msg='prepare_ena_submission_data_task. no brokerobjects. '
+#                 'return={0} '
+#                 'submission_id={1}'.format(TaskProgressReport.CANCELLED,
+#                                            submission_id)
+#         )
+#         return TaskProgressReport.CANCELLED
