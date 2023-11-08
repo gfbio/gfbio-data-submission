e03f0e78a51b9f0251772135b7408dd972273c6a
Marc Weber <mweber@gfbio.org>
2023-11-08 12:09:10 +0100

    master increase DATA_UPLOAD_MAX_MEMORY_SIZE

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,7 +186,7 @@ MEDIA_URL = "/media/"
 # UPLOAD LIMITS
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#file-upload-max-memory-size
-DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 5242880
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
