d170736a4a7ef4c48c4e748d8dc9f7bd1ef1e407
Marc Weber <mweber@gfbio.org>
2024-03-04 19:23:07 +0100

    DASS-1492 add unique constraint on field type

--- gfbio_submissions/submission_profile/models/field_type.py
+++ gfbio_submissions/submission_profile/models/field_type.py
@@ -5,8 +5,7 @@ from model_utils.models import TimeStampedModel
 
 
 class FieldType(TimeStampedModel):
-    # TODO: unique constaint ?
-    type = models.SlugField(max_length=32, blank=False, null=False)
+    type = models.SlugField(max_length=32, blank=False, null=False, unique=True)
 
     def __str__(self):
         return "{}_field_type".format(self.type)
