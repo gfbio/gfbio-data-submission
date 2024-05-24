eaa33ce90f63d6d4bd8df9ee61ddfcf48c1fcb67
Marc Weber <mweber@gfbio.org>
2024-05-24 15:25:15 +0200

    DASS-1817 add field_name to Field model

--- gfbio_submissions/submission_profile/models/field.py
+++ gfbio_submissions/submission_profile/models/field.py
@@ -7,18 +7,22 @@ from ..models.field_type import FieldType
 
 
 class Field(TimeStampedModel):
+    field_name = models.SlugField(max_length=32, blank=False, null=False,
+                                  help_text="A descriptive name for the field. this will help in differentiating and "
+                                            "selecting between fields. e.g. 'generic_title' or 'molecular_embargo_date'")
+    field_type = models.ForeignKey(FieldType, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, blank=False, null=False,
                              help_text="Title of the field, as displayed in the rendered Form")
     description = models.TextField(default="", blank=True,
                                    help_text="Descriptive text, below the title in the rendered Form")
-    field_type = models.ForeignKey(FieldType, on_delete=models.CASCADE)
+
     comment = models.TextField(default="", blank=True,
                                help_text="Comment text describing the field. This is optional. "
                                          "The information provided here WILL NOT BE SHOWN IN THE FORM")
 
     def __str__(self):
-        return "{}-{}".format(self.pk, self.field_type.type)
+        return "{}_{}".format(self.field_name, self.pk)
 
     # TODO: unique enough ?
     def field_id(self):
-        return self.__str__()
+        return "{}-{}".format(self.pk, self.field_type.type)
