c92dcfe52c38840f87ae1990bc6fbd334ed700b4
Marc Weber <mweber@gfbio.org>
2024-05-31 19:05:40 +0200

    DASS-1817 add inheritance of profiles
    - add user profile (just as an idea)

--- gfbio_submissions/submission_profile/models/field.py
+++ gfbio_submissions/submission_profile/models/field.py
@@ -15,10 +15,26 @@ class Field(TimeStampedModel):
                              help_text="Title of the field, as displayed in the rendered Form")
     description = models.TextField(default="", blank=True,
                                    help_text="Descriptive text, below the title in the rendered Form")
-
+    mandatory = models.BooleanField(default=False)
+    visible = models.BooleanField(default=True)
+    default = models.TextField(max_length=64, default="")
     comment = models.TextField(default="", blank=True,
                                help_text="Comment text describing the field. This is optional. "
                                          "The information provided here WILL NOT BE SHOWN IN THE FORM")
+    # TODO: tests/serializer auf neue Felder anpassen
+    # TODO: test for inherited profiles
+    # TODO: test for all field (inherited of inherited)
+    # DONE-TODO: mandatory, visible, defaults
+    # TODO: profile als Get parameter (ivo)
+    # DONE-TODO: inherit profile fields from other profiles
+    # TODO: json import
+    # FIXME: initiale Idee war json file als profil config, und nutzer profile in datenbank
+    #   TODO: Begründung für Entscheidung gegen File (bzw. json field).
+    # TODO: wie profil mit nutzer verbinen ?
+    #   --> UserProfile model, über url anfragen bzw. in view nach user schauen ?
+    #   - wie basis auswählen ?
+    #   - wie felder auswählen ?
+    #
 
     def __str__(self):
         return "{}_{}".format(self.field_name, self.pk)
