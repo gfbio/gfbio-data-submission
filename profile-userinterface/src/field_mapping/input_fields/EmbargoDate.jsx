0404bb9ca4997a94fd8881c16697436c93d460da
Marc Weber <mweber@gfbio.org>
2024-06-12 11:14:10 +0200

    DASS-1908 add buttons
    - add set period logic
    - add accept and cancel logic

--- profile-userinterface/src/field_mapping/input_fields/EmbargoDate.jsx
+++ profile-userinterface/src/field_mapping/input_fields/EmbargoDate.jsx
@@ -10,6 +10,7 @@ const EmbargoDate = (props) => {
     today.setFullYear(today.getFullYear() + 1);
     console.log('today +one year ', today);
     const [embargoDate, setEmbargoDate] = useState(today);
+    const [tmpEmbargoDate, setTempEmbargoDate] = useState(embargoDate);
 
     // TODO: add logic for:
     //  Do not show button if at least one PID has status PUBLIC
@@ -24,7 +25,13 @@ const EmbargoDate = (props) => {
         );
     }
 
-    const formattedEmbargoDate = ()=> {
+    const addMonthsToInitialEmbargoDate = (months) => {
+        console.log('addMonthsToEmbargoDate ', months);
+        today.setMonth(embargoDate.getMonth() + months);
+        setEmbargoDate(today);
+    }
+
+    const formattedEmbargoDate = () => {
         return (
             embargoDate.getDate().toString() + ' ' +
             embargoDate.toLocaleString('default', {month: 'long'}) + ' ' +
@@ -39,9 +46,45 @@ const EmbargoDate = (props) => {
                 <h4>{formattedEmbargoDate()}</h4>
                 {showEmbargoButton()}
             </header>
-            <Modal opened={opened} onClose={close} title="Change embargo date" centered>
-                <h4>datepicker</h4>
-                <DatePicker defaultDate={today} value={embargoDate} onChange={setEmbargoDate}/>
+            <Modal opened={opened} onClose={close} title="Select embargo date" centered>
+                <Group justify="center">
+                    <Button variant="default" onClick={() => {
+                        addMonthsToInitialEmbargoDate(6)
+                    }}>
+                        6 months
+                    </Button>
+                    <Button variant="default" onClick={() => {
+                        addMonthsToInitialEmbargoDate(12)
+                    }}>
+                        12 months
+                    </Button>
+                    <Button variant="default" onClick={() => {
+                        addMonthsToInitialEmbargoDate(18)
+                    }}>
+                        18 months
+                    </Button>
+                </Group>
+                <Group justify="center">
+                    <p>New Embargo: <b>{formattedEmbargoDate()}</b></p>
+                </Group>
+                <Group justify="center">
+                    <DatePicker defaultDate={today} value={embargoDate} onChange={setEmbargoDate}/>
+                </Group>
+                <Group justify="center">
+                    <Button variant="default" onClick={() => {
+                        setTempEmbargoDate(embargoDate);
+                        close();
+                    }}>
+                        Accept
+                    </Button>
+                    <Button variant="default" onClick={() => {
+                        setEmbargoDate(tmpEmbargoDate);
+                        close();
+                    }}>
+                        Cancel
+                    </Button>
+                </Group>
+
             </Modal>
 
         </div>
