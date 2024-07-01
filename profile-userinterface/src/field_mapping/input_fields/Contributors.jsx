4ad6d26c3872d5847c2051460ea5500fd31b3850
Tobias Hauhut <thauhut@gfbio.org>
2024-07-01 17:49:44 +0200

    add ContributorsRoles component
    This commit adds a new component called ContributorsRoles to the profile-userinterface project. The component displays a list of roles and their descriptions for contributors to a project. It uses the @mantine/core library for styling.
    
    The purpose of this component is to provide information about the different roles involved in a project, such as authors, data curators, data owners, and researchers. This information can be useful for users who want to understand the responsibilities and expertise of the contributors.
    
    This commit also includes the necessary imports, grid layout, and styling for displaying the roles and descriptions. Each role is rendered as a grid row with the role name in a bold font and the description below it.

--- profile-userinterface/src/field_mapping/input_fields/Contributors.jsx
+++ profile-userinterface/src/field_mapping/input_fields/Contributors.jsx
@@ -1,15 +1,21 @@
 import {
   Button,
   Card,
+  Collapse,
   Grid,
+  Group,
   Input,
   List,
+  Modal,
   MultiSelect,
   TextInput,
   ThemeIcon,
+  UnstyledButton,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import { useState } from "react";
+import RolesInfo from "../../utils/ContributorsRoles";
 
 const Contributors = (props) => {
   const { title, description, form, field_id } = props;
@@ -23,6 +29,9 @@ const Contributors = (props) => {
   });
   const [editingContributor, setEditingContributor] = useState(null);
   const [emailValid, setEmailValid] = useState(false);
+
+  const [opened, { toggle }] = useDisclosure(false);
+  const [rolesInfoOpened, { open, close }] = useDisclosure(false);
 
   const mainRoles = [
     "Author/Creator",
@@ -116,145 +125,178 @@ const Contributors = (props) => {
   };
 
   return (
-    <div>
-      <Input.Label>{title}</Input.Label>
-      <Input.Description>{description}</Input.Description>
-      <Grid gutter="lg">
-        <Grid.Col span={4}>
-          <Card shadow="xs" padding="sm">
-            <h4>Contributors List</h4>
-            <List
-              spacing="sm"
-              icon={
-                <ThemeIcon color="blue" variant="filled">
-                  <i className="fa fa-user-circle-o"></i>
-                </ThemeIcon>
-              }
-            >
-              {contributors.map((contributor) => (
-                <List.Item
-                  key={contributor.email}
-                  onClick={() => handleEditContributor(contributor)}
-                  style={{ cursor: "pointer" }}
+    <>
+      <div>
+        <Input.Label>{title}</Input.Label>
+        <Input.Description>{description}</Input.Description>
+        <Group justify="center" display={!opened ? "flex" : "none"}>
+          <Button onClick={toggle} color="blue">
+            Add Contributor
+          </Button>
+        </Group>
+        <Collapse in={opened}>
+          <Grid gutter="lg">
+            <Grid.Col span={4}>
+              <Card shadow="xs" padding="sm">
+                <h4>Contributors List</h4>
+                <List
+                  spacing="sm"
+                  icon={
+                    <ThemeIcon color="blue" variant="filled">
+                      <i className="fa fa-user-circle-o"></i>
+                    </ThemeIcon>
+                  }
                 >
-                  {contributor.firstName} {contributor.lastName}
-                </List.Item>
-              ))}
-            </List>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={8}>
-          <Card shadow="xs" padding="lg">
-            <h4>
-              {editingContributor ? "Edit Contributor" : "Add Contributor"}
-            </h4>
-            <Grid gutter={"sm"} style={{ marginBottom: "1rem" }}>
-              <Grid.Col span={3}>
-                <span>First Name</span>
-                <TextInput
-                  name="firstName"
-                  value={newContributor.firstName}
-                  onChange={handleInputChange}
-                />
-              </Grid.Col>
-              <Grid.Col span={3}>
-                <span>Last Name</span>
-                <TextInput
-                  name="lastName"
-                  value={newContributor.lastName}
-                  onChange={handleInputChange}
-                />
-              </Grid.Col>
-              <Grid.Col span={6}>
-                <span>Email</span>
-                <TextInput
-                  name="email"
-                  autoComplete="email"
-                  value={newContributor.email}
-                  onChange={handleInputChange}
-                />
-              </Grid.Col>
-              <Grid.Col span={12}>
-                <span>Institution (optional)</span>
-                <TextInput
-                  name="institution"
-                  value={newContributor.institution}
-                  onChange={handleInputChange}
-                />
-              </Grid.Col>
-              <Grid.Col span={12}>
-                <span>Contributor Role (optional)</span>
-                <MultiSelect
-                  name="role"
-                  value={newContributor.role}
-                  onChange={handleRoleChange}
-                  data={[
-                    { group: "Main Roles", items: [...mainRoles] },
-                    { group: "Additional Roles", items: [...additionalRoles] },
-                  ]}
-                  placeholder="Select role"
-                />
-              </Grid.Col>
-            </Grid>
-            {editingContributor ? (
-              <Grid>
-                <Grid.Col span={4}>
+                  {contributors.map((contributor) => (
+                    <List.Item
+                      key={contributor.email}
+                      onClick={() => handleEditContributor(contributor)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {contributor.firstName} {contributor.lastName}
+                    </List.Item>
+                  ))}
+                </List>
+              </Card>
+            </Grid.Col>
+            <Grid.Col span={8}>
+              <Card shadow="xs" padding="lg">
+                <h4>
+                  {editingContributor ? "Edit Contributor" : "Add Contributor"}
+                </h4>
+                <Grid gutter={"sm"} style={{ marginBottom: "1rem" }}>
+                  <Grid.Col span={3}>
+                    <span>First Name</span>
+                    <TextInput
+                      name="firstName"
+                      value={newContributor.firstName}
+                      onChange={handleInputChange}
+                    />
+                  </Grid.Col>
+                  <Grid.Col span={3}>
+                    <span>Last Name</span>
+                    <TextInput
+                      name="lastName"
+                      value={newContributor.lastName}
+                      onChange={handleInputChange}
+                    />
+                  </Grid.Col>
+                  <Grid.Col span={6}>
+                    <span>Email</span>
+                    <TextInput
+                      name="email"
+                      autoComplete="email"
+                      value={newContributor.email}
+                      onChange={handleInputChange}
+                    />
+                  </Grid.Col>
+                  <Grid.Col span={12}>
+                    <span>Institution (optional)</span>
+                    <TextInput
+                      name="institution"
+                      value={newContributor.institution}
+                      onChange={handleInputChange}
+                    />
+                  </Grid.Col>
+                  <Grid.Col span={12}>
+                    <span>
+                      Contributor Role (optional)
+                      <UnstyledButton
+                        className="fa fa-question-circle-o"
+                        onClick={open}
+                        ml={10}
+                        fz={18}
+                      ></UnstyledButton>
+                    </span>
+                    <MultiSelect
+                      name="role"
+                      value={newContributor.role}
+                      onChange={handleRoleChange}
+                      data={[
+                        { group: "Main Roles", items: [...mainRoles] },
+                        {
+                          group: "Additional Roles",
+                          items: [...additionalRoles],
+                        },
+                      ]}
+                      placeholder="Select role"
+                    />
+                  </Grid.Col>
+                </Grid>
+                {editingContributor ? (
+                  <Grid>
+                    <Grid.Col span={4}>
+                      <Button
+                        fullWidth
+                        onClick={handleSaveContributor}
+                        disabled={
+                          !newContributor.firstName ||
+                          !newContributor.lastName ||
+                          !emailValid
+                        }
+                      >
+                        Save Changes
+                      </Button>
+                    </Grid.Col>
+                    <Grid.Col span={4}>
+                      <Button
+                        fullWidth
+                        onClick={() => {
+                          setEditingContributor(null);
+                          setNewContributor({
+                            firstName: "",
+                            lastName: "",
+                            email: "",
+                            institution: "",
+                            role: [],
+                          });
+                          setEmailValid(false);
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                    </Grid.Col>
+                    <Grid.Col span={4}>
+                      <Button
+                        fullWidth
+                        onClick={() =>
+                          handleDeleteContributor(editingContributor)
+                        }
+                        color="red"
+                      >
+                        Delete
+                      </Button>
+                    </Grid.Col>
+                  </Grid>
+                ) : (
                   <Button
-                    fullWidth
-                    onClick={handleSaveContributor}
+                    onClick={handleAddContributor}
                     disabled={
                       !newContributor.firstName ||
                       !newContributor.lastName ||
                       !emailValid
                     }
                   >
-                    Save Changes
+                    Add Contributor
                   </Button>
-                </Grid.Col>
-                <Grid.Col span={4}>
-                  <Button
-                    fullWidth
-                    onClick={() => {
-                      setEditingContributor(null);
-                      setNewContributor({
-                        firstName: "",
-                        lastName: "",
-                        email: "",
-                        institution: "",
-                        role: [],
-                      });
-                      setEmailValid(false);
-                    }}
-                  >
-                    Cancel
-                  </Button>
-                </Grid.Col>
-                <Grid.Col span={4}>
-                  <Button
-                    fullWidth
-                    onClick={() => handleDeleteContributor(editingContributor)}
-                    color="red"
-                  >
-                    Delete
-                  </Button>
-                </Grid.Col>
-              </Grid>
-            ) : (
-              <Button
-                onClick={handleAddContributor}
-                disabled={
-                  !newContributor.firstName ||
-                  !newContributor.lastName ||
-                  !emailValid
-                }
-              >
-                Add Contributor
-              </Button>
-            )}
-          </Card>
-        </Grid.Col>
-      </Grid>
-    </div>
+                )}
+              </Card>
+            </Grid.Col>
+          </Grid>
+        </Collapse>
+      </div>
+
+      <Modal
+        opened={rolesInfoOpened}
+        onClose={close}
+        title="Roles Description"
+        size="50%"
+        yOffset="10vh"
+        scrollAreaComponent={"ScrollArea.Autosize"}
+      >
+        <RolesInfo />
+      </Modal>
+    </>
   );
 };
 
