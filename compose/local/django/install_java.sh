714178e11e3e7dd73135bb447320ba602f6b4ff6
Marc Weber <mweber@gfbio.org>
2024-10-07 10:53:57 +0200

    DASS-2095 update local compose file

--- compose/local/django/install_java.sh
+++ compose/local/django/install_java.sh
@@ -3,7 +3,7 @@
 sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
 apt-get update
 apt-get install -y curl
-curl -LfsSo /tmp/openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jre_x64_linux_hotspot_8u252b09.tar.gz
+curl -LfsSo /tmp/openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk17-binaries/archive/refs/tags/jdk-2021-05-07-13-31.tar.gz
 mkdir -p /opt/java/openjdk
 cd /opt/java/openjdk
 tar -xf /tmp/openjdk.tar.gz --strip-components=1
