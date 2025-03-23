ace0b436d0ed014e32bc2f92ffc0524115dc68b0
Marc Weber <mweber@gfbio.org>
2025-03-23 21:45:15 +0100

    DASS-2397 mock s3 download 3 files for atax tasks

--- gfbio_submissions/brokerage/tasks/atax_tasks/atax_run_combination_task.py
+++ gfbio_submissions/brokerage/tasks/atax_tasks/atax_run_combination_task.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
+from django.conf import settings
+from abcd_converter_gfbio_org import abcd_conversion, handlers, file_validation
+from dt_upload.views.backend_based_upload_mixins import get_s3_client
 
 from config.celery_app import app
 from ...configuration.settings import SUBMISSION_MAX_RETRIES, SUBMISSION_RETRY_DELAY
 from ...exceptions.transfer_exceptions import TransferServerError, TransferClientError
-from ...models.submission import Submission
 from ...models.abcd_conversion_result import AbcdConversionResult
+from ...models.submission import Submission
 from ...models.submission_upload import SubmissionUpload
+from ...models.submission_cloud_upload import SubmissionCloudUpload
 from ...tasks.submission_task import SubmissionTask
-
-from abcd_converter_gfbio_org import abcd_conversion, handlers, file_validation
 
 logger = logging.getLogger(__name__)
 
@@ -33,17 +36,6 @@ def atax_run_combination_task(
     )
     submission = Submission.objects.get(pk=submission_id)
     submission_upload_files = SubmissionUpload.objects.filter(submission_id=submission_id)
-    print('submission_upload_files ', submission_upload_files)
-    print(submission_upload_files.filter(file__icontains="specimen"))
-    print(submission_upload_files.filter(file__icontains="measurement"))
-    print(submission_upload_files.filter(file__icontains="multimedia"))
-
-    # if submission_upload_files empty, try same but for cloud_uploads
-    # download locally like in attach, use local path then
-    # check below on file content ?
-    # OR .. better copy the whole thing, create unit test class like for attach
-    #   and do everything with locally downloaded files
-
 
     spec_file = submission_upload_files.filter(file__icontains="specimen").first()
     measurements_file = submission_upload_files.filter(file__icontains="measurement").first()
@@ -51,12 +43,12 @@ def atax_run_combination_task(
 
     if not spec_file or not measurements_file or not multimedia_file:
         AbcdConversionResult.objects.create(
-            submission = submission,
-            atax_xml_valid = False,
-            xml = "",
-            errors = "No files found.",
-            warnings = "",
-            logs = "",
+            submission=submission,
+            atax_xml_valid=False,
+            xml="",
+            errors="No files found.",
+            warnings="",
+            logs="",
         )
         return False
 
@@ -72,29 +64,125 @@ def atax_run_combination_task(
 
     atax_xml_valid = False
     try:
-        xml = abcd_conversion.convert_csv_to_abcd(spec_file.file.path, measurements_file.file.path, multimedia_file.file.path, handlings)
+        xml = abcd_conversion.convert_csv_to_abcd(spec_file.file.path, measurements_file.file.path,
+                                                  multimedia_file.file.path, handlings)
         atax_xml_valid = True if xml else False
 
         AbcdConversionResult.objects.create(
-            submission = submission,
-            atax_xml_valid = atax_xml_valid,
-            xml = handlings.resultFileHandler.result[0]["content"],
-            errors = error_handler.result,
-            warnings = handlings.warning_handler.result,
-            logs = handlings.logHandler.result,
+            submission=submission,
+            atax_xml_valid=atax_xml_valid,
+            xml=handlings.resultFileHandler.result[0]["content"],
+            errors=error_handler.result,
+            warnings=handlings.warning_handler.result,
+            logs=handlings.logHandler.result,
         )
     except Exception as exc:
         error_handler.result.append(exc.with_traceback(None))
 
         AbcdConversionResult.objects.create(
-            submission = submission,
-            atax_xml_valid = False,
-            xml = handlings.resultFileHandler.result[0]["content"] if len(handlings.resultFileHandler.result) > 0 else "",
-            errors = error_handler.result,
-            warnings = handlings.warning_handler.result,
-            logs = handlings.logHandler.result,
+            submission=submission,
+            atax_xml_valid=False,
+            xml=handlings.resultFileHandler.result[0]["content"] if len(handlings.resultFileHandler.result) > 0 else "",
+            errors=error_handler.result,
+            warnings=handlings.warning_handler.result,
+            logs=handlings.logHandler.result,
         )
 
+    return atax_xml_valid
+
+
+@app.task(
+    base=SubmissionTask,
+    bind=True,
+    name="tasks.atax_run_combination_for_cloud_upload_task",
+    autoretry_for=(TransferServerError, TransferClientError),
+    retry_kwargs={"max_retries": SUBMISSION_MAX_RETRIES},
+    retry_backoff=SUBMISSION_RETRY_DELAY,
+    retry_jitter=True,
+)
+def atax_run_combination_for_cloud_upload_task(
+    self,
+    previous_task_result=None,
+    submission_id=None,
+):
+    logger.info(
+        "tasks.py | atax_run_combination_for_cloud_upload_task"
+    )
+    submission = Submission.objects.get(pk=submission_id)
+    submission_upload_files = SubmissionCloudUpload.objects.filter(submission_id=submission_id)
+    # print('submission_upload_files ', submission_upload_files)
+    print(submission_upload_files.filter(file_upload__original_filename__icontains="specimen"))
+    print(submission_upload_files.filter(file_upload__original_filename__icontains="measurement"))
+    print(submission_upload_files.filter(file_upload__original_filename__icontains="multimedia"))
+
+    spec_file = submission_upload_files.filter(file_upload__original_filename__icontains="specimen").first()
+    measurements_file = submission_upload_files.filter(file_upload__original_filename__icontains="measurement").first()
+    multimedia_file = submission_upload_files.filter(file_upload__original_filename__icontains="multimedia").first()
+
+    if not spec_file or not measurements_file or not multimedia_file:
+        AbcdConversionResult.objects.create(
+            submission=submission,
+            atax_xml_valid=False,
+            xml="",
+            errors="No files found.",
+            warnings="",
+            logs="",
+        )
+        return False
+
+    error_handler = ToFieldOutputter()
+    handlings = handlers.InOutHandler()
+    handlings.dataProvider = DataFromSubmissionProvider(submission)
+    handlings.resultFileHandler = ToFieldOutputter()
+    handlings.errorHandler = error_handler
+    handlings.warning_handler = ToFieldOutputter()
+    handlings.logHandler = ToFieldOutputter()
+    handlings.singleFileHandler = ToFieldOutputter()
+    handlings.multimedia_validator = SubmissionMultimediaFileValidator(submission.id, handlings)
+
+    atax_xml_valid = False
+
+    bucket_name, s3_client = get_s3_client()
+    local_spec_file = f"local-{spec_file.file_upload.file_key}"
+    with open(local_spec_file, "wb") as f:
+        s3_client.download_fileobj(Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+                                   Key=spec_file.file_upload.file_key, Fileobj=f)
+    local_measurements_file = f"local-{measurements_file.file_upload.file_key}"
+    with open(local_measurements_file, "wb") as f:
+        s3_client.download_fileobj(Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+                                   Key=measurements_file.file_upload.file_key, Fileobj=f)
+    local_multimedia_file = f"local-{multimedia_file.file_upload.file_key}"
+    with open(local_multimedia_file, "wb") as f:
+        s3_client.download_fileobj(Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+                                   Key=multimedia_file.file_upload.file_key, Fileobj=f)
+    try:
+        xml = abcd_conversion.convert_csv_to_abcd(local_spec_file, local_measurements_file,
+                                                  local_multimedia_file, handlings)
+        atax_xml_valid = True if xml else False
+
+        AbcdConversionResult.objects.create(
+            submission=submission,
+            atax_xml_valid=atax_xml_valid,
+            xml=handlings.resultFileHandler.result[0]["content"],
+            errors=error_handler.result,
+            warnings=handlings.warning_handler.result,
+            logs=handlings.logHandler.result,
+        )
+    except Exception as exc:
+        error_handler.result.append(exc.with_traceback(None))
+
+        AbcdConversionResult.objects.create(
+            submission=submission,
+            atax_xml_valid=False,
+            xml=handlings.resultFileHandler.result[0]["content"] if len(handlings.resultFileHandler.result) > 0 else "",
+            errors=error_handler.result,
+            warnings=handlings.warning_handler.result,
+            logs=handlings.logHandler.result,
+        )
+
+    os.remove(local_spec_file)
+    os.remove(local_measurements_file)
+    os.remove(local_multimedia_file)
 
     return atax_xml_valid
 
@@ -104,7 +192,7 @@ class ToFieldOutputter(handlers.Outputter):
         self.result = []
 
     def handle(self, description, content):
-        self.result.append({ "description": description, "content": content })
+        self.result.append({"description": description, "content": content})
 
 
 class DataFromSubmissionProvider(handlers.DataProvider):
@@ -124,24 +212,30 @@ class DataFromSubmissionProvider(handlers.DataProvider):
 class SubmissionMultimediaFileValidator(file_validation.MultimediaFileValidatorInterface):
     def __init__(self, submission_id, io_handler):
         self.io_handler = io_handler
-        submission_upload_list = list(SubmissionUpload.objects.filter(submission_id=submission_id).values_list("file", flat=True).all())
+        submission_upload_list = list(
+            SubmissionUpload.objects.filter(submission_id=submission_id).values_list("file", flat=True).all())
         self.submission_upload_list = [entry.split("/")[1] for entry in submission_upload_list]
         self.file_extensions = file_validation.FILE_EXTENSIONS
         self.file_extensions["image"].append("tif")
 
     def validate(self, file_name, format, row):
         if not file_name:
-            self.io_handler.warning_handler.handle(f"File in row {row} has no name.", { "file": "multimedia", "row": row, "message": "File has no name"})
+            self.io_handler.warning_handler.handle(f"File in row {row} has no name.",
+                                                   {"file": "multimedia", "row": row, "message": "File has no name"})
             return
 
         if not format:
-            self.io_handler.warning_handler.handle(f"File in row {row} has no format.", { "file": "multimedia", "row": row, "message": "File has no format"})
+            self.io_handler.warning_handler.handle(f"File in row {row} has no format.",
+                                                   {"file": "multimedia", "row": row, "message": "File has no format"})
         else:
             file_extension = file_name.rsplit(".")[1]
-            if (format.lower() not in self.file_extensions and format.lower() != file_extension.lower()) or (format.lower() in self.file_extensions and file_extension.lower() not in self.file_extensions[format.lower()]):
+            if (format.lower() not in self.file_extensions and format.lower() != file_extension.lower()) or (
+                format.lower() in self.file_extensions and file_extension.lower() not in self.file_extensions[
+                format.lower()]):
                 msg = f"File extension '{file_extension}' of {file_name} may not match the format description '{format}'."
-                self.io_handler.warning_handler.handle(msg, { "file": "multimedia", "row": row, "message": "Unrecognized file extension"})
+                self.io_handler.warning_handler.handle(msg, {"file": "multimedia", "row": row,
+                                                             "message": "Unrecognized file extension"})
 
         if not file_name.replace(" ", "_") in self.submission_upload_list:
             msg = f"File {file_name} in row {row} is missing it's corresponding file in the upload."
-            self.io_handler.errorHandler.handle(msg, { "file": "multimedia", "row": row, "message": "File not found"})
+            self.io_handler.errorHandler.handle(msg, {"file": "multimedia", "row": row, "message": "File not found"})
