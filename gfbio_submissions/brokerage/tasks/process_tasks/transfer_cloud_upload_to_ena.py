c63e0bf8c815359cbd494e424f1afc05842f2284
Marc Weber <mweber@gfbio.org>
2025-04-24 12:33:21 +0200

    DASS-2602 set up s3fs mount
    - add s3fs volume to local.yml
    - add mount of bucket to local start script for django
    - add upload envs to local env file for django
    - install s3fs in local Dockerfile
    - copy local s3 password file in local Dockerfile
    - comment aspera installation for now in local Dockerfile

--- gfbio_submissions/brokerage/tasks/process_tasks/transfer_cloud_upload_to_ena.py
+++ gfbio_submissions/brokerage/tasks/process_tasks/transfer_cloud_upload_to_ena.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
+import tempfile
 
 from config.celery_app import app
 from ...models import SubmissionCloudUpload
 from ...models.task_progress_report import TaskProgressReport
+from dt_upload.views.backend_based_upload_mixins import get_s3_client
 
 logger = logging.getLogger(__name__)
 
@@ -31,9 +34,33 @@ def transfer_cloud_upload_to_ena_task(self, previous_result=None, submission_clo
             "no valid SubmissionCloudUpload available | "
             "submission_cloud_upload_id={0}".format(submission_cloud_upload_id))
         return TaskProgressReport.CANCELLED
-    print("download temp file for cloud upload", submission_cloud_upload)
-    print("transfer via ascp")
+    print("\n\nprocess upload: ", submission_cloud_upload)
+    print(submission_cloud_upload.file_upload.original_filename)
+    print(submission_cloud_upload.file_upload.file_key)
+    print(submission_cloud_upload.file_upload.s3_location)
+    print("check s3 mount -------------------")
+    path = "/mnt/s3bucket"
+    file_path = f"{path}{os.path.sep}{submission_cloud_upload.file_upload.file_key}"
+    print(os.path.exists(path))
+    print(os.path.exists(file_path))
+    if os.path.exists(file_path):
+        print(file_path)
+        print(os.path.getsize(file_path))
+    else:
+        print("no found ", file_path)
+    # bucket_name, s3_client = get_s3_client()
+    # with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+    #     s3_client.download_fileobj(
+    #         Bucket=bucket_name,
+    #         Key=submission_cloud_upload.file_upload.file_key,
+    #         Fileobj=tmp_file
+    #     )
+    #     local_path = tmp_file.name
+    #     print("Loca PATH", local_path)
+    #     print("size ", os.path.getsize(local_path))
+    print("\n\n-----------------------------------\n\ntransfer via ascp")
     print("deal with return values")
     print("save result to Requestlog & TaskProgressReport")
     print("delete temp file")
+    # os.remove(local_path)
     return TaskProgressReport.CANCELLED
