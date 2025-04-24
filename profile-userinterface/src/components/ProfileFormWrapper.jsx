fb442c50679c02d6065d2876868a640bb8e42816
linus <lfranz@gfbio.org>
2025-04-24 17:43:09 +0200

    DASS-2684 Put profile-switch-button in the sub-navi

--- profile-userinterface/src/components/ProfileFormWrapper.jsx
+++ profile-userinterface/src/components/ProfileFormWrapper.jsx
@@ -5,19 +5,30 @@ import useFetchProfileAndSubmission from "../hooks/useFetchProfileAndSubmission.
 import { DEFAULT_PROFILE_NAME } from "../settings.jsx";
 import ProfileForm from "./ProfileForm.jsx";
 import { useState } from "react";
+import { Button } from "@mantine/core";
 import { modals } from "@mantine/modals";
+import Cookies from 'universal-cookie';
+
+import useFetchProfileList from "../hooks/useFetchProfileList.jsx";
+import ProfileSelector from "./ProfileSelector.jsx";
+import NavigationMenu from "./NavigationMenu.jsx";
 
 const ProfileWithLoading = withLoading(ProfileForm);
 const ProfileWithErrorHandling = withErrorHandling(ProfileWithLoading);
 
 const ProfileFormWrapper = () => {
+
     const brokerSubmissionId = useParams().brokerageId;
-    const profileName = localStorage.getItem("profileName") || DEFAULT_PROFILE_NAME;
+    const cookies = new Cookies();
+cookies.set('myCat', 'Pacman', { path: '/' });
+console.log(cookies.get('myCat')); // Pacman
+    const profileName = cookies.get("profileName") || DEFAULT_PROFILE_NAME;
 
     const [activeProfile, setActiveProfile] = useState(profileName);
 
     const handleProfileChange = (data) => {
-        localStorage.setItem("profileName", data);
+        cookies.set("profileName", data, { path: '/', expires: new Date(Date.now() + 365 / 2 * 86400 * 1000) });
+        
         modals.closeAll();
         setActiveProfile(data);
     };
@@ -34,29 +45,50 @@ const ProfileFormWrapper = () => {
         localSubmissionFiles,
     } = useFetchProfileAndSubmission(activeProfile, brokerSubmissionId);
 
+    const {
+        profileListData,
+        profileListIsLoading,
+        profileListError,
+    } = useFetchProfileList();
+    
     // TODO: where display errors ? what actions if error ?
     return (
         <>
-            {/* TODO: DASS-2455 (12.03.2025): this button opens a modal with a dialog that allows selecting on of the
-                    available system-wide-profile followed by a re-render of the form based on the newly selected
-                    profile. This is de-activated for now, but will be used in the near future. */}
-            {/*<Button*/}
-            {/*    onClick={() => {*/}
-            {/*        modals.open({*/}
-            {/*            title: "Submission Profile Selection",*/}
-            {/*            size: "xl",*/}
-            {/*            centered: true,*/}
-            {/*            children: (*/}
-            {/*                <>*/}
-            {/*                    <ProfileSelector onCancel={modals.closeAll} onProfileChange={handleProfileChange}></ProfileSelector>*/}
-            {/*                </>*/}
-            {/*            ),*/}
-            {/*        });*/}
-            {/*    }}*/}
-            {/*>*/}
-            {/*    Change Profile*/}
-            {/*</Button>*/}
-            {/* -----------------------------------------------------------------------------------------------------*/}
+            <NavigationMenu>
+                {
+                    profileListData && profileListData.length > 1 &&
+                    <div className="nav-link ms-auto change-profile-button-menu" 
+                        onClick={() => {
+                            modals.open({
+                                title: "Submission Profile Selection",
+                                size: "xl",
+                                centered: true,
+                                children: (
+                                    <>
+                                        <ProfileSelector 
+                                            onCancel={modals.closeAll}
+                                            onProfileChange={handleProfileChange}
+                                            profileListData={profileListData}
+                                            activeProfile={activeProfile} />
+                                    </>
+                                ),
+                            });
+                        }}
+                    >
+                        <div className="me-2">
+                            <div className="fs-7">
+                                Fields displayed according to
+                            </div>
+                            <div className="fs-7">
+                                profile <b>{profileData?.name ?? profileName}</b>
+                            </div>
+                        </div>
+                        <Button className="change-profile-button">
+                            Change
+                        </Button>
+                    </div>
+                }
+            </NavigationMenu>
 
             <div id={"profileFormWrapper"}>
                 <ProfileWithErrorHandling
--- profile-userinterface/src/components/SubmissionList.jsx
+++ profile-userinterface/src/components/SubmissionList.jsx
@@ -5,6 +5,7 @@ import {useEffect, useState} from "react";
 import {Link, useLoaderData, useLocation} from "react-router-dom";
 import deleteSubmission from "../api/deleteSubmission";
 import SimpleModal from "./simpleModal";
+import NavigationMenu from './NavigationMenu.jsx';
 
 // SubmissionList component
 const SubmissionList = (props) => {
@@ -75,110 +76,113 @@ const SubmissionList = (props) => {
     };
 
     return (
-        <div className="submission-list-wrapper">
-            <Collapse in={isSuccessVisible}>
-                <div className="col-8 mx-auto success-message">
-                    <div className="row">
-                        <div className="col-1 mx-auto">
-                            <i className="icon ion-md-checkmark-circle-outline"/>
-                        </div>
-                        <div className="col-8">
-                            <h4>{successHeader}</h4>
-                            <p>{successText}</p>
-                        </div>
-                        <div className="col-2">
-                            <button
-                                className="btn btn-sm w-100 btn-green-inverted"
-                                onClick={handleClose}
-                            >
-                                Close
-                            </button>
+        <>
+            <NavigationMenu />
+            <div className="submission-list-wrapper">
+                <Collapse in={isSuccessVisible}>
+                    <div className="col-8 mx-auto success-message">
+                        <div className="row">
+                            <div className="col-1 mx-auto">
+                                <i className="icon ion-md-checkmark-circle-outline"/>
+                            </div>
+                            <div className="col-8">
+                                <h4>{successHeader}</h4>
+                                <p>{successText}</p>
+                            </div>
+                            <div className="col-2">
+                                <button
+                                    className="btn btn-sm w-100 btn-green-inverted"
+                                    onClick={handleClose}
+                                >
+                                    Close
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Collapse>
+                </Collapse>
 
-            {submissions.length === 0 ? (
-                <div className="list-start-wrapper d-flex">
-                    <div className="container my-auto">
-                        <div className="row g-0 text-center">
-                            <div className="col-md-10 ps-3 align-middle">
-                                <Link to={formUrl} className="nav-link list-start">
-                                    <p>You have no submissions yet.</p>
-                                    <p>Start a new submission</p>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <div className="pt-3">
-                    <div className="row g-0">
-                        <div className="col-md-10 ps-3">
-                            <div className="row g-0">
-                                <div className="col-md-8 align-self-center">
-                                    <h6>Title</h6>
-                                </div>
-                                <div className="col-md-2 align-self-center">
-                                    <h6>Status</h6>
-                                </div>
-                                <div className="col-md-2 align-self-center">
-                                    <h6>Ticket</h6>
+                {submissions.length === 0 ? (
+                    <div className="list-start-wrapper d-flex">
+                        <div className="container my-auto">
+                            <div className="row g-0 text-center">
+                                <div className="col-md-10 ps-3 align-middle">
+                                    <Link to={formUrl} className="nav-link list-start">
+                                        <p>You have no submissions yet.</p>
+                                        <p>Start a new submission</p>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <ul className="list-group">
-                        {submissions.map((submission) => (
-                            <li
-                                key={submission.broker_submission_id}
-                                className="list-group-item"
-                            >
+                ) : (
+                    <div className="pt-3">
+                        <div className="row g-0">
+                            <div className="col-md-10 ps-3">
                                 <div className="row g-0">
-                                    <div className="col-md-10">
-                                        <Link
-                                            to={formUrl + submission.broker_submission_id}
-                                            className="row g-0"
-                                        >
-                                            <div className="col-md-8 col-sm-12 align-self-center">
-                                                <i className="icon ion-md-apps"/>
-                                                <span>{submission.data.requirements.title}</span>
-                                            </div>
-                                            <div className="col-md-2 col-sm-12 align-self-center status">
-                                                <span>{submission.status}</span>
-                                            </div>
-                                            <div className="col-md-2 col-sm-12 align-self-center">
-                                                <span className="issue">{submission.issue}</span>
-                                            </div>
-                                        </Link>
+                                    <div className="col-md-8 align-self-center">
+                                        <h6>Title</h6>
                                     </div>
-                                    <div className="col-md-2 col-sm-12 align-self-center actions">
-                                        <Link
-                                            to={formUrl + submission.broker_submission_id}
-                                            className="action h-100 d-inline-block pe-4 btn btn-link"
-                                        >
-                                            <i className="icon ion-md-create"/> Edit
-                                        </Link>
-                                        <a
-                                            className="action h-100 d-inline-block btn btn-link"
-                                            onClick={() => handleDeleteClick(submission)}
-                                        >
-                                            <i className="icon ion-md-trash"/>Delete
-                                        </a>
+                                    <div className="col-md-2 align-self-center">
+                                        <h6>Status</h6>
+                                    </div>
+                                    <div className="col-md-2 align-self-center">
+                                        <h6>Ticket</h6>
                                     </div>
                                 </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-            <SimpleModal
-                isOpen={isDeleteModalOpen}
-                onClose={handleDeleteCancel}
-                onConfirm={handleDeleteConfirm}
-                itemName={submissionToDelete?.data.requirements.title || ""}
-            />
-        </div>
+                            </div>
+                        </div>
+                        <ul className="list-group">
+                            {submissions.map((submission) => (
+                                <li
+                                    key={submission.broker_submission_id}
+                                    className="list-group-item"
+                                >
+                                    <div className="row g-0">
+                                        <div className="col-md-10">
+                                            <Link
+                                                to={formUrl + submission.broker_submission_id}
+                                                className="row g-0"
+                                            >
+                                                <div className="col-md-8 col-sm-12 align-self-center">
+                                                    <i className="icon ion-md-apps"/>
+                                                    <span>{submission.data.requirements.title}</span>
+                                                </div>
+                                                <div className="col-md-2 col-sm-12 align-self-center status">
+                                                    <span>{submission.status}</span>
+                                                </div>
+                                                <div className="col-md-2 col-sm-12 align-self-center">
+                                                    <span className="issue">{submission.issue}</span>
+                                                </div>
+                                            </Link>
+                                        </div>
+                                        <div className="col-md-2 col-sm-12 align-self-center actions">
+                                            <Link
+                                                to={formUrl + submission.broker_submission_id}
+                                                className="action h-100 d-inline-block pe-4 btn btn-link"
+                                            >
+                                                <i className="icon ion-md-create"/> Edit
+                                            </Link>
+                                            <a
+                                                className="action h-100 d-inline-block btn btn-link"
+                                                onClick={() => handleDeleteClick(submission)}
+                                            >
+                                                <i className="icon ion-md-trash"/>Delete
+                                            </a>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <SimpleModal
+                    isOpen={isDeleteModalOpen}
+                    onClose={handleDeleteCancel}
+                    onConfirm={handleDeleteConfirm}
+                    itemName={submissionToDelete?.data.requirements.title || ""}
+                />
+            </div>
+        </>
     );
 };
 
