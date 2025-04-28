04d1c8e0e2e5187750cd2160e5ac6734a3a7ee73
Marc Weber <mweber@gfbio.org>
2025-04-28 15:17:19 +0200

    DASS-2602 adapt ci scripts

--- cicd/updateStaging.sh
+++ cicd/updateStaging.sh
@@ -5,6 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 # script
 rm -r .envs && cp -r /home/gitlab-runner/.gfbio_envs/ .envs
+mv .envs/passwd-s3fs compose/production/django/passwd-s3fs
 sed -i "s/VERSION =.*/VERSION ='$(git describe --tags | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')'/g" config/settings/base.py
 nvm use 8
 cd userinterface && npm i && npm run collect-ci
