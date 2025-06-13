1bf66b515220bfdea5a6d4199b6d5cec5bcd07fe
Marc Weber <mweber@gfbio.org>
2025-06-13 11:08:03 +0200

    master change ascp path

--- gfbio_submissions/brokerage/tasks/process_tasks/transfer_cloud_upload_to_ena.py
+++ gfbio_submissions/brokerage/tasks/process_tasks/transfer_cloud_upload_to_ena.py
@@ -33,7 +33,7 @@ def perform_ascp_file_transfer(task, file_path, site_configuration, submission):
     # according to: https://ena-docs.readthedocs.io/en/latest/submit/fileprep/upload.html#using-aspera-ascp-command-line-program
     aspera_host = site_configuration.ena_aspera_server.url
     aspera_user = site_configuration.ena_aspera_server.username
-    aspera_target_path = f"{submission.broker_submission_id}/."
+    aspera_target_path = f"/{submission.broker_submission_id}/"
     remote_dest = f"{aspera_user}@{aspera_host}:{aspera_target_path}"
     cmd = [settings.ASPERA_ASCP_PATH, "-QT", "-l", "100M", file_path, remote_dest]
     logger.info(f"tasks.py | transfer_cloud_upload_to_ena_task | execute cmd={cmd} | task_id={task.request.id}")
