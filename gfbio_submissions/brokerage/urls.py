a570ab97142075cbe5ee0316483d5600cd09b2d3
linus <lfranz@gfbio.org>
2025-06-18 15:00:07 +0200

    DASS-2738 Added download for files and links in the UI

--- gfbio_submissions/brokerage/urls.py
+++ gfbio_submissions/brokerage/urls.py
@@ -11,6 +11,7 @@ from .views.submission_cloud_upload_view import SubmissionCloudUploadAbortView
 from .views.submission_cloud_upload_view import SubmissionCloudUploadCompleteView
 from .views.submission_cloud_upload_view import SubmissionCloudUploadUpdatePartView
 from .views.submission_cloud_upload_view import SubmissionCloudUploadView, SubmissionCloudUploadPartURLView
+from .views.submission_cloud_upload_download_view import SubmissionCloudGetDownloadLinkView, SubmissionCloudZipAllFilesAndDownload
 from .views.submission_comment_view import SubmissionCommentView
 from .views.submission_detail_view import SubmissionDetailView
 from .views.submission_report_view import SubmissionReportView
@@ -87,6 +88,16 @@ urlpatterns = [
         route=r"submissions/(?P<broker_submission_id>[0-9a-z-]+)/cloud-upload/patch/(?P<pk>[0-9]+)/$",
         view=SubmissionCloudUploadPatchView.as_view(),
         name="submissions_cloud_upload_patch",
+    ),
+    re_path(
+        route="submissions/(?P<broker_submission_id>[0-9a-z-]+)/cloudupload/download_file/(?P<file_id>[0-9]+)/$",
+        view=SubmissionCloudGetDownloadLinkView.as_view(),
+        name="submissions_cloud_file_download"
+    ),
+    re_path(
+        route="submissions/(?P<broker_submission_id>[0-9a-z-]+)/cloudupload/zip/$",
+        view=SubmissionCloudZipAllFilesAndDownload.as_view(),
+        name="submissions_cloud_zip_download"
     ),
     re_path(
         route=r"submissions/(?P<broker_submission_id>[0-9a-z-]+)/comment/$",
