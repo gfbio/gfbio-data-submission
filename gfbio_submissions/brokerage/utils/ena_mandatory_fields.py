8bc5df7483a6ce6cf9db0f41f3b53e4bdc36df99
Jakob Schwabauer <jschwabauer@gfbio.org>
2026-05-28 09:21:16 +0200

    DASS-3521 small removal of now unneeded alias

--- gfbio_submissions/brokerage/utils/ena_mandatory_fields.py
+++ gfbio_submissions/brokerage/utils/ena_mandatory_fields.py
@@ -23,8 +23,6 @@ PAIRED_MANDATORY_FIELDS = [
     "reverse_read_file_checksum",
 ]
 
-SINGLE_FORBIDDEN_FIELDS = PAIRED_MANDATORY_FIELDS
-
 FIELD_HELP_TEXT = {
     "sample_title": (
         "A unique label for your samples, preferably one you can use to map to any other data "
@@ -236,7 +234,7 @@ def validate_ena_mandatory_fields(csv_file):
                         }
                     )
         elif layout == "single":
-            for field_name in SINGLE_FORBIDDEN_FIELDS:
+            for field_name in PAIRED_MANDATORY_FIELDS:
                 if field_name not in present_fields:
                     continue
                 if _has_value(row.get(field_name)):
