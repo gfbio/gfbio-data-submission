1d223b4238bd0c5850c00403b81acf92b59088fa
Claas-Thido Pfaff <claas-thido.pfaff@gfbio.org>
2026-06-02 14:37:19 +0200

    refactor: migrate process_tasks to submission_task decorator
    Registered Celery task names are unchanged.
    
    Refs DASS-3565

--- gfbio_submissions/brokerage/tasks/process_tasks/validate_against_ena.py
+++ gfbio_submissions/brokerage/tasks/process_tasks/validate_against_ena.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
-from config.celery_app import app
-from ...configuration.settings import SUBMISSION_MAX_RETRIES, SUBMISSION_RETRY_DELAY
-from ...exceptions.transfer_exceptions import TransferServerError, TransferClientError
-from ...tasks.submission_task import SubmissionTask
+from ...tasks.submission_task import submission_task
 from ...utils.task_utils import send_data_to_ena_for_validation_or_test
 
 
-@app.task(
-    base=SubmissionTask,
-    bind=True,
-    name="tasks.validate_against_ena_task",
-    autoretry_for=(TransferServerError, TransferClientError),
-    retry_kwargs={"max_retries": SUBMISSION_MAX_RETRIES},
-    retry_backoff=SUBMISSION_RETRY_DELAY,
-    retry_jitter=True,
-)
+@submission_task("tasks.validate_against_ena_task")
 def validate_against_ena_task(self, submission_id=None, action="VALIDATE"):
     results = send_data_to_ena_for_validation_or_test(self, submission_id, action)
     return results
