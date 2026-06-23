fedbc48e2b0e94367e3fa88d19484c8c53a95df1
linus <lfranz@gfbio.org>
2026-06-23 14:37:17 +0200

    DASS-3951 fix tests

--- gfbio_submissions/brokerage/tests/test_utils/test_calculate_checksum.py
+++ gfbio_submissions/brokerage/tests/test_utils/test_calculate_checksum.py
@@ -7,15 +7,24 @@ from gfbio_submissions.brokerage.utils.s3fs import calculate_checksum_locally
 
 
 class TestChecksumUtils(TestCase):
-    def test_md5_calculation(self):
+    @classmethod
+    def setUpTestData(cls):
+        super(TestChecksumUtils, cls).setUpTestData()
+        cls.original_mnt_point = settings.S3FS_MOUNT_POINT
         settings.S3FS_MOUNT_POINT = "/app/gfbio_submissions/brokerage/tests/test_data/jpg_files/"
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestChecksumUtils, cls).tearDownClass()
+        settings.S3FS_MOUNT_POINT = cls.original_mnt_point
+
+    def test_md5_calculation(self):
         fu = FileUploadRequest(file_key="Holotype_FGZC3761.jpg")
         scu = SubmissionCloudUpload(file_upload=fu)
         result = calculate_checksum_locally("md5", scu)
         self.assertEqual("debf8a985fc8d6b99a5b12ef76a0df59", result)
 
     def test_sha_calculation(self):
-        settings.S3FS_MOUNT_POINT = "/app/gfbio_submissions/brokerage/tests/test_data/jpg_files/"
         fu = FileUploadRequest(file_key="Holotype_FGZC3761.jpg")
         scu = SubmissionCloudUpload(file_upload=fu)
         result = calculate_checksum_locally("sha256", scu)
